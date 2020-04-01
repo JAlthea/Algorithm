08b5c1b3e421c03414e7efd84695c3dfbdd75b6a
Yongmin <althea_@naver.com>
2020-04-02 04:25:09 +0900

    Create BOJ 1697 숨바꼭질.cpp

---
+++ BFS/BOJ 1697 숨바꼭질.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    queue<pair<int, int>> q;
+    q.push({ 0, n });
+    int check[100001] = { 0 };
+    while (!q.empty())
+    {
+        pair<int, int> p = q.front(); q.pop();
+        if (p.second == k)
+        {
+            cout << p.first;
+            return 0;
+        }
+        if (check[p.second])
+            continue;
+        check[p.second]++;
+        
+        int nx[] = { p.second + 1, p.second - 1, p.second * 2 };
+        for (int i=0; i<3; i++)
+            if (nx[i] >= 0 && nx[i] <= 100000)
+                q.push({ p.first + 1, nx[i] });
+    }
+}
