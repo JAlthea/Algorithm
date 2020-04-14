09aab6d731a086caf0ace1d0d4e743ba5762f473
Yongmin <althea_@naver.com>
2020-04-15 01:15:58 +0900

    Update Programmers 완주하지 못한 선수.cpp

--- Hash/Programmers 완주하지 못한 선수.cpp
+++ Hash/Programmers 완주하지 못한 선수.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 using namespace std;
 
+//Answer1
 string solution(vector<string> participant, vector<string> completion)
 {
     sort(participant.begin(), participant.end());
@@ -10,3 +11,6 @@ string solution(vector<string> participant, vector<string> completion)
         if (participant[i] != completion[i])
             return participant[i];
 }
+
+//Answer2
+//participant의 원소들을 completion을 넣은 set을 검사하면서 존재하지 않으면 그 원소를 리턴시키는 방법
