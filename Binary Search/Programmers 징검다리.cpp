5da3620fa9f034d9efd6dd93e7f6dcbe0a91f934
Yongmin <althea_@naver.com>
2020-04-19 00:45:43 +0900

    Create Programmers 징검다리.cpp

---
+++ Binary Search/Programmers 징검다리.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+    sort(rocks.begin(), rocks.end());
+    int s = 1;
+    int e = distance;
+    int mid;
+    int answer = 0;
+    
+    while (s <= e)
+    {
+        mid = (s + e) / 2;
+        int count = 0;
+        int prev = 0;
+        for (int i=0; i<rocks.size(); i++)
+        {
+            if (rocks[i] - prev < mid)
+                count++;
+            else
+                prev = rocks[i];
+        }
+        if (distance - prev < mid)
+            count++;
+        
+        if (count <= n)
+        {
+            answer = max(answer, mid);
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+    }
+    
+    return answer;
+}
