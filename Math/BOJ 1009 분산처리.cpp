abca7078e897bee9e1e13ff4f4fbd24b2adfcfa8
Yongmin <althea_@naver.com>
2020-05-24 02:43:16 +0900

    Update BOJ 1009 분산처리.cpp

--- Math/BOJ 1009 분산처리.cpp
+++ Math/BOJ 1009 분산처리.cpp
@@ -4,7 +4,7 @@ int main()
 	int t;
 	scanf("%d", &t);
 	while (t--)
-    {
+	{
 		int a, b, c;
 		scanf("%d%d", &a, &b);
 
@@ -13,7 +13,6 @@ int main()
 
 		for (int i = 2; i <= b; ++i) 
 			c = (c * a) % 10;
-
 		printf("%d\n", c ? c : 10);
 	}
 }
