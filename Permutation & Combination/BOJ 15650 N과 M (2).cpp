eb31830e958e98374baaa8fb47cfaa789e8410c4
Yongmin <althea_@naver.com>
2020-05-30 02:47:43 +0900

    Create BOJ 15650 N과 M (2).cpp

---
+++ Permutation & Combination/BOJ 15650 N과 M (2).cpp
@@ -0,0 +1,31 @@
+#include <ios>
+
+int n, m, d[9];
+bool check[9];
+
+void f(int pos)
+{
+	if (m == pos)
+	{
+		for (int i = 0; i < m; i++)
+			printf("%d ", d[i]);
+		return;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (!check[i] && (pos == 0 || d[pos - 1] < i))
+		{
+			check[i] = true;
+			d[pos] = i;
+			f(pos + 1);
+			check[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	scanf("%d%d", &n, &m);
+	f(0);
+}
