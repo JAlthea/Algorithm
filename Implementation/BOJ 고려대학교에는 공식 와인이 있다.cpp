a793e31678871e0067b1148de85e5a9d632aa814
Yongmin <althea_@naver.com>
2020-06-10 00:45:55 +0900

    Delete BOJ 고려대학교에는 공식 와인이 있다.cpp

--- Implementation/BOJ 고려대학교에는 공식 와인이 있다.cpp
+++
@@ -1,9 +0,0 @@
-#include <ios>
-int main()
-{
-    int c, k, p, count = 0;
-    scanf("%d%d%d", &c, &k, &p);
-    for (int i=1; i<=c; i++)
-        count += k * i + p * i * i;
-    printf("%d", count);
-}
