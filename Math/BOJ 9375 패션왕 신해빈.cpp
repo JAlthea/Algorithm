b70b5a2274551c663099134c6330dc82f026f07b
Yongmin <althea_@naver.com>
2020-08-22 00:24:04 +0900

    Update BOJ 9375 패션왕 신해빈.cpp

--- Math/BOJ 9375 패션왕 신해빈.cpp
+++ Math/BOJ 9375 패션왕 신해빈.cpp
@@ -10,7 +10,7 @@ int main()
         map<string, int> m;
         string s;
         while(n--)
-            cin >> s >> s, m.find(s) == m.end() ? m[s] = 1 : m[s]++;
+            cin >> s >> s, m.find(s) == m.end() ? m[s] = 1 : ++m[s];
         n = 1;
         for(auto i=m.begin(); i!=m.end(); ++i)
             n *= 1 + (*i).second;
