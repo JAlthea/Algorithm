e099efdfca939545cc363bddc2bb39bb91fe144d
Yongmin <althea_@naver.com>
2020-09-22 18:39:11 +0900

    Create BOJ 10815 숫자 카드.cpp

---
+++ Binary Search/BOJ 10815 숫자 카드.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+	cin >> m;
+	vector<int> b(m);
+	for (int i = 0; i < m; ++i)
+		cin >> b[i];
+	sort(a.begin(), a.end());
+	for (int i = 0; i < m; ++i)
+		cout << (binary_search(a.begin(), a.end(), b[i]) == true ? "1 " : "0 ");
+}
