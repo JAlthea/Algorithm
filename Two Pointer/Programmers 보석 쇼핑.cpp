139e1d00b135e145113cbde547f86502beac7ed0
Yongmin <althea_@naver.com>
2020-09-24 01:20:04 +0900

    Update Programmers 보석 쇼핑.cpp

--- Two Pointer/Programmers 보석 쇼핑.cpp
+++ Two Pointer/Programmers 보석 쇼핑.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> solution(vector<string> gems) {
-    vector<int> answer;
+vector<int> solution(vector<string> gems)
+{
+    vector<int> answer(2);
     unordered_map<string, int> m;
     set<string> s;
     int n = gems.size();
-    for (int i=0; i<n; i++)
+    for (int i=0; i<n; ++i)
         s.insert(gems[i]);
 
     int allGemSize = s.size();
     int minStart = 0;
-    int minEnd = (int)1e9;
+    int minEnd = INT_MAX;
     int start = 0;
     int end = 0;
-    for (int i=0; i<n; i++)
+    for (int i=0; i<n; ++i)
     {
         m[gems[end++]]++;
         while (m[gems[start]] >= 2)
@@ -27,7 +28,7 @@ vector<int> solution(vector<string> gems) {
         }
     }
     
-    answer.push_back(minStart);
-    answer.push_back(minEnd);
+    answer.front() = minStart;
+    answer.back() = minEnd;
     return answer;
 }
