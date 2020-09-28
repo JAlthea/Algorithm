1a5b47e3f566f0e3e603437127fce75d1d80c9bc
Yongmin <althea_@naver.com>
2020-09-29 06:32:14 +0900

    Create BOJ 1927번 최소 힙.cpp

---
+++ Heap/BOJ 1927번 최소 힙.cpp
@@ -0,0 +1,27 @@
+#include <queue>
+#include <ios>
+using namespace std;
+
+int main()
+{
+    int n, x;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    scanf("%d", &n);
+    for (int i=0; i<n; ++i)
+    {
+        scanf("%d", &x);
+        if (x)
+            pq.push(x);
+        else
+        {
+            if (pq.empty())
+            {
+                puts("0");
+                continue;
+            }
+
+            printf("%d\n", pq.top());
+            pq.pop();
+        }
+    }
+}
