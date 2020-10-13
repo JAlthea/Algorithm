96b1cd22fb903f0dffafa285f20b233aaa011ef4
Yongmin <althea_@naver.com>
2020-10-13 19:26:39 +0900

    Create BOJ 1753 최단경로.cpp

---
+++ Dijkstra Algorithm/BOJ 1753 최단경로.cpp
@@ -0,0 +1,45 @@
+#include<ios>
+#include<vector>
+#include<queue>
+using namespace std;
+int main()
+{
+	int n, m, start;
+	scanf("%d%d%d", &n, &m, &start);
+	vector<vector<pair<int, int>>> v(n+1);
+	while (m--)
+	{
+		int a, b, c;
+		scanf("%d%d%d", &a, &b, &c);
+		v[a].push_back({ b, c });
+	}
+	vector<int> dist(n+1, 999999);
+
+	priority_queue<pair<int, int>> pq;
+	pq.push(make_pair(0, start));
+	dist[start] = 0;
+	while (!pq.empty())
+	{
+		int cost = -1 * pq.top().first;
+		int here = pq.top().second;
+		pq.pop();
+
+		for (int i = 0; i < v[here].size(); i++)
+		{
+			int next = v[here][i].first;
+			int nextCost = v[here][i].second;
+
+			if (dist[next] > dist[here] + nextCost)
+			{
+				dist[next] = dist[here] + nextCost;
+				pq.push(make_pair(-1 * dist[next], next));
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+		if (dist[i] == 999999)
+			puts("INF");
+		else
+			printf("%d\n", dist[i]);
+}    
