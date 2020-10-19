0e13f83bd9ba0950767b4a6b84094ba30c29f2eb
Yongmin <althea_@naver.com>
2020-10-20 08:05:12 +0900

    Create BOJ 11723 집합.cpp

---
+++ Bit Mask/BOJ 11723 집합.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	int m, x, s = 0;
+	cin >> m;
+	while (m--)
+	{
+		string c;
+		cin >> c;
+		if (c == "all") s = (1 << 21) - 1;
+		else if (c == "empty") s = 0;
+		else cin >> x;
+		if (c == "add") s |= (1 << x);
+		else if (c == "check") cout << ((s & (1 << x)) ? "1\n" : "0\n");
+		else if (c == "remove") s &= ~(1 << x);
+		else if (c == "toggle") s ^= (1 << x);
+	}
+}
