3aca3825f9c53a99cf19d01fdf80c21d1f7a93f7
Yongmin <althea_@naver.com>
2020-12-06 18:11:19 +0900

    Update BOJ 2839 설탕 배달.cpp

--- Math/BOJ 2839 설탕 배달.cpp
+++ Math/BOJ 2839 설탕 배달.cpp
@@ -6,16 +6,16 @@ int f(int n)
 {
 	if (n == 0) return 0;
 	if (n < 3) return -1;
-	if (d[n]) return d[n];
+	if (d[n] > 0) return d[n];
 
-	int c1 = f(n - 5);
-	int c2 = f(n - 3);
-	if (c1 != -1) c1++;
-	if (c2 != -1) c2++;
+	int c1 = n >= 5 ? f(n - 5) : 0;
+	int c2 = n >= 3 ? f(n - 3) : 0;
+	if (n >= 5 && c1 != -1) c1++;
+	if (n >= 3 && c2 != -1) c2++;
  
-	if (c1 && c2) d[n] = c1 < c2 ? c1 : c2;
-	else if (c1) d[n] = c1;
-	else if (c2) d[n] = c2;
+	if (c1 > 0 && c2 > 0) d[n] = c1 < c2 ? c1 : c2;
+	else if (c1 > 0) d[n] = c1;
+	else if (c2 > 0) d[n] = c2;
 	else d[n] = -1;
 
 	return d[n];
