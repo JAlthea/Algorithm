01f9d26670846ab07639fe87568dae29eccba0c4
Yongmin <althea_@naver.com>
2020-12-13 05:39:39 +0900

    Update BOJ 1978 소수 찾기.cpp

--- Math/BOJ 1978 소수 찾기.cpp
+++ Math/BOJ 1978 소수 찾기.cpp
@@ -1,13 +1,15 @@
 #import <ios>
-int n, i, j, c, d[1001] = {0, 1};
+int n, i, j, c, d[1001];
 int main()
 {
-    for(i=2; i*i<=1000; i++)
-        if(!d[i])
-            for(j=i*i; j<1001; j+=i)
-                d[j]++;
-    scanf("%d", &n);
-    while (n--)
-        scanf("%d", &i), !d[i] ? c++ : c;
-    printf("%d", c);
+	d[1]++;
+	for (i = 2; i * i <= 1000; ++i)
+		if (!d[i])
+			for (j = i * i; j < 1001; j += i)
+				d[j]++;
+  
+	scanf("%d", &n);
+	while (n--)
+		scanf("%d", &i), !d[i] ? c++ : c;
+	printf("%d", c);
 }
