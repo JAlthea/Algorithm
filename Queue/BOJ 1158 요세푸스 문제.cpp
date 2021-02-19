ea675d11d099b8405b3cd4977b1cacb8fb0be646
Yongmin <althea_@naver.com>
2021-02-20 00:17:28 +0900

    Create BOJ 1158 요세푸스 문제.cpp

---
+++ Queue/BOJ 1158 요세푸스 문제.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    
+    queue<int> q;
+    for (int i=1; i<=n; ++i)
+        q.push(i);
+    
+    cout << '<';
+    while (1)
+    {
+        for (int i=0; i<k-1; ++i)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        q.pop();
+        
+        if (q.empty()) break;
+        cout << ", ";
+    }
+    cout << '>';
+}
