74b9359fa5986442d090ec584ed9b48360757e7c
Yongmin Jang <althea_@naver.com>
2021-04-11 00:16:18 +0900

    Create BOJ 17256 달달함이 넘쳐흘러.cpp

---
+++ Math/BOJ 17256 달달함이 넘쳐흘러.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main() {
+    int ax, ay, az, cx, cy, cz;
+    scanf("%d %d %d\n%d %d %d", &ax, &ay, &az, &cx, &cy, &cz);
+    printf("%d %d %d", cx - az, cy / ay, cz - ax);
+}
