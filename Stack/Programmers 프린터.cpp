6340c908013789e8b76131dea23d99144c4bda3e
JAlthea <althea_@naver.com>
2021-05-02 00:11:43 +0900

    Update Programmers 프린터.cpp

--- Stack/Programmers 프린터.cpp
+++ Stack/Programmers 프린터.cpp
@@ -5,32 +5,32 @@ using namespace std;
 int solution(vector<int> priorities, int location) {
     vector<int> tmp = priorities;
     vector<vector<int>> s(tmp.size(), vector<int>(2));
-    for (int i=0; i<tmp.size(); i++)
-    {
+    for (int i = 0; i < tmp.size(); ++i) {
         s[i][0] = tmp[i];
         s[i][1] = i;
     }
+
     sort(tmp.begin(), tmp.end(), greater<int>());
     
     int k = 0;
     int count = 0;
-    while (!tmp.empty())
-    {
+    
+    while (!tmp.empty()) {
         vector<int> v = s.front();
         
-        if (v[0] == tmp[k])
-        {
+        if (v[0] == tmp[k]) {
             s.erase(s.begin());
-            count++;
-            k++;
+            ++count;
+            ++k;
+
             if (v[1] == location)
                 break;
+
+            continue;
         }
-        else
-        {
-            s.erase(s.begin());
-            s.push_back(v);  
-        }
+        
+        s.erase(s.begin());
+        s.push_back(v);  
     }
     
     return count;
