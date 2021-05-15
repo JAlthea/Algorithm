928dcc0f7966b0d92eb3dfcf726813257944adb7
Yongmin Jang <althea_@naver.com>
2021-05-15 15:03:48 +0900

    Delete BOJ 1934 최소공배수.cpp

--- BOJ 1934 최소공배수.cpp
+++
@@ -1,9 +0,0 @@
-#import <ios>
-int p, q, n, t;
-int GCD(int a, int b) { return b ? GCD(b, a % b) : a; }
-int main()
-{
-  scanf("%d", &t);
-  while (t--)
-    scanf("%d%d", &p, &q), n = p > q ? GCD(p, q) : GCD(q, p), printf("%d\n", p * q / n);
-}
