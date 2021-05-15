ab3357234f7ceae84e647dd24fd4ebaa7120789d
Yongmin Jang <althea_@naver.com>
2021-05-15 20:47:00 +0900

    Update BOJ 9663 N-Queen.cpp

--- BackTracking/BOJ 9663 N-Queen.cpp
+++ BackTracking/BOJ 9663 N-Queen.cpp
@@ -2,50 +2,37 @@
 
 int d[15], size, count;
 
-bool check(int i, int j)
-{
-    for (int y = i - 1; y > 0; --y)
-    {
+bool check(int i, int j) {
+    for (int y = i - 1; y > 0; --y) {
         if (d[y] == j || abs(j - d[y]) == i - y)
-        {
             return false;
-        }
     }
+
     return true;
 }
 
-void nQueen()
-{
-    int i, j;
-    for (i = 1, j = 1; i > 0;)
-    {
-        if (check(i, j))
-        {
+void nQueen() {
+    int i = 1, j = 1;
+    while (i) {
+        if (check(i, j)) {
             d[i] = j;
             j = 1;
             if (++i <= size) 
                 continue;
-        }
-        else if (++j <= size)
-        {
+        } else if (++j <= size)
             continue;
-        }
         
-        if (i > size)
-        {
+        if (i > size) {
             count++;
             j = d[--i] + 1;
         }
         
         while (j > size)
-        {
             j = d[--i] + 1;
-        }
     }
 }
 
-int main()
-{
+int main() {
     scanf("%d", &size);
     nQueen();
     printf("%d", count);
