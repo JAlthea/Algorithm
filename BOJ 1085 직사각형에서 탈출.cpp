095ad4a4e5cc621d71e89c2fa01e3e7608c6581a
Yongmin Jang <althea_@naver.com>
2021-05-15 14:59:56 +0900

    Delete BOJ 1085 직사각형에서 탈출.cpp

--- BOJ 1085 직사각형에서 탈출.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-int x, y, w, h, a, b;
-int main()
-{
-    scanf("%d%d%d%d", &x, &y, &w, &h);
-    a = x < w - x ? x : w - x;
-    b = y < h - y ? y : h - y;
-    printf("%d", a < b ? a : b);
-}
