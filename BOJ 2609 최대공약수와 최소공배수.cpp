71063c4640c09a86ad0bc0e8ff08de35a3a9ee58
Yongmin Jang <althea_@naver.com>
2021-05-15 15:02:45 +0900

    Delete BOJ 2609 최대공약수와 최소공배수.cpp

--- BOJ 2609 최대공약수와 최소공배수.cpp
+++
@@ -1,9 +0,0 @@
-#import <ios>
-int n, p, q;
-int GCD(int a, int b) { return a % b ? GCD(b, a % b) : b; }
-int main()
-{
-    scanf("%d%d", &p, &q);
-    n = GCD(p, q);
-    printf("%d %d", n, p * q / n);
-}
