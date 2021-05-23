125fa17db61719c115ade70cca32ba65b1e93c77
Yongmin Jang <althea_@naver.com>
2021-05-24 00:04:15 +0900

    Create BOJ 2467 용액.cpp

---
+++ Binary Search/BOJ 2467 용액.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+
+	int twoSum = INT_MAX;
+	int a, b;
+	for (int i = 0; i < n - 1; ++i) {
+		int k = arr[i];
+		int pos = lower_bound(arr.begin() + i + 1, arr.end(), -k) - arr.begin();
+		if (pos == n)
+			--pos;
+		
+		if (twoSum > abs(arr[pos] + k)) {
+			twoSum = abs(arr[pos] + k);
+			a = k;
+			b = arr[pos];
+		}
+		if (arr[pos - 1] != k && twoSum > abs(arr[pos - 1] + k)) {
+			twoSum = abs(arr[pos - 1] + k);
+			a = k;
+			b = arr[pos - 1];
+		}
+	}
+
+	cout << a << ' ' << b;
+}
