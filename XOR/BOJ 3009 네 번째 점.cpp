767e969f9759a5de818c11747e7520b8215652eb
Yongmin Jang <althea_@naver.com>
2021-06-09 00:56:18 +0900

    Update BOJ 3009 네 번째 점.cpp

--- XOR/BOJ 3009 네 번째 점.cpp
+++ XOR/BOJ 3009 네 번째 점.cpp
@@ -1,8 +1,9 @@
 #include <ios>
+
 int x, y, a, b;
-int main()
-{
-	while (~scanf("%d %d", &a, &b))
+int main() {
+	while (~scanf("%d %d", &a, &b)) {
 		x ^= a, y ^= b;
+	}
 	printf("%d %d", x, y);
 }
