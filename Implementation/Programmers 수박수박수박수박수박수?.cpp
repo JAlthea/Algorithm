ed9b2994b41a179b4ceb950454af1c47d02e5bd3
Yongmin Jang <althea_@naver.com>
2021-07-06 00:05:54 +0900

    Update Programmers 수박수박수박수박수박수?.cpp

--- Implementation/Programmers 수박수박수박수박수박수?.cpp
+++ Implementation/Programmers 수박수박수박수박수박수?.cpp
@@ -4,6 +4,6 @@ using namespace std;
 string solution(int n) {
     string s = "";
     for (int i = 0; i < n; ++i)
-        s += i % 2 ? "박" : "수";
+        s += i & 1 ? "박" : "수";
     return s;
 }
