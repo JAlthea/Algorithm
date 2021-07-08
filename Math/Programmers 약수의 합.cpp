15bd57a273df17bdf8eed85813fa91b87f01313d
Yongmin Jang <althea_@naver.com>
2021-07-09 01:22:51 +0900

    Create Programmers 약수의 합.cpp

---
+++ Math/Programmers 약수의 합.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+
+int solution(int n) {
+    int answer = n;
+    for (unsigned short i = 1; i <= n / 2; ++i)
+        if (n % i == 0)
+            answer += i;
+    return answer;
+}
