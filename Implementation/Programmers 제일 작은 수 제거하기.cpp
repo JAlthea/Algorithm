801c69d8620bb91ed406ab986960df8636c9cbc8
Yongmin Jang <althea_@naver.com>
2021-07-17 01:20:45 +0900

    Create Programmers 제일 작은 수 제거하기.cpp

---
+++ Implementation/Programmers 제일 작은 수 제거하기.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    
+    int min = *min_element(arr.begin(), arr.end());
+    for (int i = 0; i < arr.size(); ++i)
+        if (arr[i] != min)
+            answer.push_back(arr[i]);
+    
+    if (answer.empty())
+        answer.push_back(-1);
+  
+    return answer;
+}
