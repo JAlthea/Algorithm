4bd29025b99e4438a6f1225d16ded05001d0aa67
Yongmin Jang <althea_@naver.com>
2021-09-18 01:51:42 +0900

    Update Programmers 카카오프렌즈 컬러링북.cpp

--- BFS/Programmers 카카오프렌즈 컬러링북.cpp
+++ BFS/Programmers 카카오프렌즈 컬러링북.cpp
@@ -14,33 +14,32 @@ vector<int> solution(int m, int n, vector<vector<int>> picture) {
     const int dx[] = { 0, 1, 0, -1 };
     vector<vector<char>> visited(m, vector<char>(n, 0));
     vector<int> area;
-    for (int i=0; i<m; i++)
-    {
-        for (int j=0; j<n; j++)
-        {
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
             if (visited[i][j] || !picture[i][j])
                 continue;
             
             queue<Point> q;
-            q.push({j, i, picture[i][j]});
+            q.push({ j, i, picture[i][j] });
             int count = 0;
-            while (!q.empty())
-            {
+            while (!q.empty()) {
                 Point p = q.front(); q.pop();
                 
                 if (visited[p.y][p.x])
                     continue;
-                visited[p.y][p.x]++;
-                count++;
                 
-                for (int i=0; i<4; i++)
-                {
+                ++visited[p.y][p.x];
+                ++count;
+                
+                for (int i = 0; i < 4; ++i) {
                     int ny = p.y + dy[i];
                     int nx = p.x + dx[i];
+                    
                     if (ny < 0 || nx < 0 || ny >= m || nx >= n)
                         continue;
+                    
                     if (picture[ny][nx] == p.value)
-                        q.push({nx, ny, p.value});
+                        q.push({ nx, ny, p.value });
                 }
             }
             
@@ -48,8 +47,5 @@ vector<int> solution(int m, int n, vector<vector<int>> picture) {
         }
     }
     
-    vector<int> answer(2);
-    answer[0] = area.size();
-    answer[1] = *max_element(area.begin(), area.end());
-    return answer;
+    return { area.size(), *max_element(area.begin(), area.end()) };
 }
