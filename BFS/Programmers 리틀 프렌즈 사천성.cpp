220263ccc37556c7d0166af15ea1d7f7878dac2b
Yongmin Jang <althea_@naver.com>
2021-09-19 15:54:22 +0900

    Update Programmers 리틀 프렌즈 사천성.cpp

--- BFS/Programmers 리틀 프렌즈 사천성.cpp
+++ BFS/Programmers 리틀 프렌즈 사천성.cpp
@@ -12,16 +12,13 @@ struct Direction {
 
 string solution(int m, int n, vector<string> board) {
     vector<vector<pair<int, int>>> tiles(26);
-    for (int i=0; i<m; i++)
-        for (int j=0; j<n; j++)
-            if (isalpha(board[i][j]))
-                tiles[board[i][j] - 'A'].push_back({j, i});
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            if (isalpha(board[i][j])) tiles[board[i][j] - 'A'].push_back({ j, i });
 
     string answer = "";
-    for (int i=0; i<26; i++)
-    {
-        if (tiles[i].empty())
-            continue;
+    for (int i = 0; i < 26; ++i) {
+        if (tiles[i].empty()) continue;
 
         char startAlpha = i + 'A';
         int startX = tiles[i][0].first;
@@ -29,9 +26,8 @@ string solution(int m, int n, vector<string> board) {
         int goalX = tiles[i][1].first;
         int goalY = tiles[i][1].second;
         queue<pair<pair<int, int>, Direction>> q;
-        q.push({tiles[i][0], {0, 0, 0, 0, 0}});
-        while (!q.empty())
-        {
+        q.push({ tiles[i][0], { 0, 0, 0, 0, 0 } });
+        while (!q.empty()) {
             pair<int, int> p = q.front().first;
             Direction d = q.front().second;
             q.pop();
@@ -39,11 +35,9 @@ string solution(int m, int n, vector<string> board) {
             int y = p.second;
             char alpha = board[p.second][p.first];
 
-            if (d.count > 2)
-                continue;
+            if (d.count > 2) continue;
 
-            if ((goalX == x && goalY == y) && startAlpha == alpha)
-            {
+            if ((goalX == x && goalY == y) && startAlpha == alpha) {
                 board[y][x] = '.';
                 board[startY][startX] = '.';
                 answer += alpha;
@@ -52,46 +46,34 @@ string solution(int m, int n, vector<string> board) {
                 break;
             }
 
-            if (board[y][x] != '.' && board[y][x] != startAlpha)
-                continue;
+            if (board[y][x] != '.' && board[y][x] != startAlpha) continue;
 
-            if (d.left == 0 && x < n - 1)
-            {
-                Direction nextD = {0, 0, 0, 1, d.count};
-                if (!d.right)
-                    nextD.count++;
-                q.push({{x + 1, y}, nextD});
+            if (d.left == 0 && x < n - 1) {
+                Direction nextD = { 0, 0, 0, 1, d.count };
+                if (!d.right) ++nextD.count;
+                q.push({ { x + 1, y }, nextD });
             }
-            if (d.right == 0 && x > 0)
-            {
-                Direction nextD = {0, 0, 1, 0, d.count};
-                if (!d.left)
-                    nextD.count++;
-                q.push({{x - 1, y}, nextD}); 
+            if (d.right == 0 && x > 0) {
+                Direction nextD = { 0, 0, 1, 0, d.count };
+                if (!d.left) ++nextD.count;
+                q.push({ { x - 1, y }, nextD }); 
             }           
-            if (d.bottom == 0 && y > 0)
-            {
-                Direction nextD = {1, 0, 0, 0, d.count};
-                if (!d.top)
-                    nextD.count++;
-                q.push({{x, y - 1}, nextD});
+            if (d.bottom == 0 && y > 0) {
+                Direction nextD = { 1, 0, 0, 0, d.count };
+                if (!d.top) ++nextD.count;
+                q.push({ { x, y - 1 }, nextD });
             }
-            if (d.top == 0 && y < m - 1)
-            {
-                Direction nextD = {0, 1, 0, 0, d.count};
-                if (!d.bottom)
-                    nextD.count++;
-                q.push({{x, y + 1}, nextD});
+            if (d.top == 0 && y < m - 1) {
+                Direction nextD = { 0, 1, 0, 0, d.count };
+                if (!d.bottom) ++nextD.count;
+                q.push({ { x, y + 1 }, nextD });
             }
         }
     }
 
     bool isEmpty = true;
-    for (int i=0; i<26; i++)
-        if (!tiles[i].empty())
-            isEmpty = false;
+    for (int i = 0; i < 26; ++i)
+        if (!tiles[i].empty()) isEmpty = false;
 
-    if (isEmpty)
-        return answer;
-    return "IMPOSSIBLE";
+    return isEmpty ? answer : "IMPOSSIBLE";
 }
