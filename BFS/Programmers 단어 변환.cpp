2febab74e97c691a36bd8892930fcc6b56f822c3
Yongmin Jang <althea_@naver.com>
2021-09-29 02:42:36 +0900

    Create Programmers 단어 변환.cpp

---
+++ BFS/Programmers 단어 변환.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(string begin, string target, vector<string> words) {
+    bool check = false;
+    for (string i : words)
+        if (i.find(target) != string::npos) 
+            check = true;
+    if (!check)
+        return 0;
+    
+    queue<pair<string, int>> q;
+    q.push({ begin, 0 });
+    while (!q.empty()) {
+        pair<string, int> p = q.front(); q.pop();
+        if (p.first.find(target) != string::npos) 
+            return p.second;
+        
+        for (int i = 0; i < words.size(); ++i) {
+            string now = words[i];
+            int count = 0;
+            
+            for (int j = 0; j < now.size(); ++j)
+                if (p.first[j] == now[j])
+                    ++count;
+            
+            if (count == now.size() - 1)
+                q.push({ now, p.second + 1 });
+        }
+    }
+    
+    return 0;
+}
