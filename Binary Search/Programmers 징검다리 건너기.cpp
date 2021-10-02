16302a62e08f7497cbc047f4afd1be7800c1ba57
Yongmin Jang <althea_@naver.com>
2021-10-03 02:07:46 +0900

    Create Programmers 징검다리 건너기.cpp

---
+++ Binary Search/Programmers 징검다리 건너기.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> stones, int k) {
+    int answer = 0;
+    int size = stones.size();
+    long long s = 1;
+    long long e = *max_element(stones.begin(), stones.end());
+    long long m;
+    
+    while (s <= e) {
+        m = (s + e) / 2;
+        int count = 0;
+        bool isPossible = true;
+        
+        for (int i = 0; i < size; ++i) {
+            if (stones[i] < m)
+                ++count;    //이게 연속으로 k번 만큼 있으면 건널 수 없다는 판정.
+            else
+                count = 0;
+            
+            if (count == k) {
+                isPossible = false;
+                break;
+            }
+        }
+
+        if (isPossible) {
+            if (answer < m)
+                answer = m;
+            s = m + 1;
+        }
+        else
+            e = m - 1;
+    }
+
+    return answer;
+}
