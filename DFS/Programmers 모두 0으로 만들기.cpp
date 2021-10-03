93b87b031b36b249dba1eb3a11f71ec7597adc1a
Yongmin Jang <althea_@naver.com>
2021-10-04 01:54:14 +0900

    Create Programmers 모두 0으로 만들기.cpp

---
+++ DFS/Programmers 모두 0으로 만들기.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> d[300000];
+vector<long long> c;
+bool isCheck[300000];
+long long answer;
+
+long long dfs(int start) {
+    long long parentValue = c[start];
+    long long ret = 0;
+    for (int i = 0; i < d[start].size(); ++i) {
+        int end = d[start][i];
+        if (isCheck[end])
+            continue;
+        
+        isCheck[end] = true;
+        long long childValues = dfs(end);
+        parentValue += childValues;
+    }
+    
+    answer += abs(parentValue);
+    return c[start] = parentValue;
+}
+
+//greedy + dfs?
+long long solution(vector<int> a, vector<vector<int>> edges) {
+    for (auto & i : a) {
+        c.push_back(i);
+    }
+    
+    for (int i = 0; i < edges.size(); ++i) {
+        int start = edges[i][0];
+        int end = edges[i][1];
+        d[start].push_back(end);
+        d[end].push_back(start);
+    }
+    
+    isCheck[0] = true;
+    if (dfs(0) + c[0])
+        return -1;
+    return answer;
+}
