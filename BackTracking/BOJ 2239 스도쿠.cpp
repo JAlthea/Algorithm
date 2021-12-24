c4a980f00bd6954a2171d8c76672f91453c4577a
JAlthea <althea_@naver.com>
2021-12-25 00:12:01 +0900

    Modify code style

--- BackTracking/BOJ 2239 스도쿠.cpp
+++ BackTracking/BOJ 2239 스도쿠.cpp
@@ -6,66 +6,64 @@ char d[9][9];
 bool v[9][9];
 bool isSuccess = false;
 
-bool isSudoku(int y, int x, char value) {
-	for (int i = 0; i < 9; i++) {
+bool isSudoku(int y, int x, char value) 
+{
+	for (int i = 0; i < 9; ++i) 
 		if (value == d[i][x])
 			return false;
-	}
 
-	for (int i = 0; i < 9; i++) {
+	for (int i = 0; i < 9; ++i) 
 		if (value == d[y][i])
 			return false;
-	}
 
 	int ny = y / 3 * 3;
 	int nx = x / 3 * 3;
-	for (int i = ny; i < ny + 3; i++) {
-		for (int j = nx; j < nx + 3; j++) {
+	for (int i = ny; i < ny + 3; ++i) 
+		for (int j = nx; j < nx + 3; ++j)
 			if (value == d[i][j])
 				return false;
-		}
-	}
 
 	return true;
 }
 
-void backTrackingSudoku(int y, int x, int count) {
-	if (count == maxCount) {
+void backTrackingSudoku(int y, int x, int count) 
+{
+	if (count == maxCount) 
+	{
 		isSuccess = true;
 		return;
 	}
 	
-	if (!v[y][x]) {
-		if (x == 8)
-			backTrackingSudoku(y + 1, 0, count);
-		else
-			backTrackingSudoku(y, x + 1, count);
+	if (!v[y][x]) 
+	{
+		x == 8 ? backTrackingSudoku(y + 1, 0, count) : backTrackingSudoku(y, x + 1, count);
+		return;
 	}
-	else {
-		for (int k = 1; k <= 9; ++k) {
-			if (isSudoku(y, x, k + '0'))
-				d[y][x] = k + '0';
-			else
-				continue;
+	
+	for (int k = 1; k <= 9; ++k) 
+	{
+		if (!isSudoku(y, x, k + '0'))
+			continue;
+		
+		d[y][x] = k + '0';
+		x == 8 ? backTrackingSudoku(y + 1, 0, count + 1) : backTrackingSudoku(y, x + 1, count + 1);
 
-			if (x == 8)
-				backTrackingSudoku(y + 1, 0, count + 1);
-			else
-				backTrackingSudoku(y, x + 1, count + 1);
-
-			if (isSuccess)
-				return;
-		}
-
-		d[y][x] = '0';
+		if (isSuccess)
+			return;
 	}
+
+	d[y][x] = '0';
 }
 
-int main() {
-	for (int i = 0; i < 9; ++i) {
-		for (int j = 0; j < 9; ++j) {
+int main() 
+{
+	for (int i = 0; i < 9; ++i) 
+	{
+		for (int j = 0; j < 9; ++j) 
+		{
 			cin >> d[i][j];
-			if (d[i][j] == '0') {
+			if (d[i][j] == '0') 
+			{
 				++maxCount;
 				v[i][j] = true;
 			}
@@ -74,9 +72,7 @@ int main() {
 
 	backTrackingSudoku(0, 0, 0);
 
-	for (int i = 0; i < 9; ++i) {
+	for (int i = 0; i < 9; cout << '\n', ++i)
 		for (int j = 0; j < 9; ++j)
 			cout << d[i][j];
-		cout << '\n';
-	}
 }
