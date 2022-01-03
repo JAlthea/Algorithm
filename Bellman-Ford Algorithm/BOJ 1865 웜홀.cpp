a1121a3aa01f0ce2366b821ef4076beead285fe9
JAlthea <althea_@naver.com>
2022-01-04 02:00:12 +0900

    Modify code style

--- Bellman-Ford Algorithm/BOJ 1865 웜홀.cpp
+++ Bellman-Ford Algorithm/BOJ 1865 웜홀.cpp
@@ -5,15 +5,18 @@ int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int tc, n, m, w, s, e, t;
 	cin >> tc;
+
 	while (tc--) {
 		cin >> n >> m >> w;
 		vector<long long> dist(n + 1, INT_MAX);
 		vector<vector<pair<long long, long long>>> edge(n + 1);
+
 		for (int i = 0; i < m; ++i) {
 			cin >> s >> e >> t;
 			edge[s].push_back({ e, t });
 			edge[e].push_back({ s, t });
 		}
+
 		for (int i = 0; i < w; ++i) {
 			cin >> s >> e >> t;
 			edge[s].push_back({ e, -t });
@@ -26,15 +29,15 @@ int main() {
 					int to = edge[from][j].first;
 					int cost = edge[from][j].second;
                 
-						if (dist[to] > dist[from] + cost) {
-							dist[to] = dist[from] + cost;
-							if (i == n - 1)
-								isPossible = true;
-						}
+					if (dist[to] > dist[from] + cost) {
+						dist[to] = dist[from] + cost;
+						if (i == n - 1)
+							isPossible = true;
 					}
 				}
 			}
-        
-			cout << (isPossible ? "YES\n" : "NO\n");
 		}
+        
+		cout << (isPossible ? "YES\n" : "NO\n");
+	}
 }
