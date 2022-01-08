f6c6c4cc178cef4b7afc7f00b861e467f158b1b9
JAlthea <althea_@naver.com>
2022-01-09 00:02:01 +0900

    Modify code style

--- BFS/BOJ 1707 이분그래프.cpp
+++ BFS/BOJ 1707 이분그래프.cpp
@@ -1,64 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+
+int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int k, v, e;
 	cin >> k;
-	while (k--)
-	{
+
+	while (k--) {
 		cin >> v >> e;
 		vector<vector<int>> s(v + 1);
-		for (int i = 0; i < e; i++)    //Connecting Info
-		{
+
+		for (int i = 0; i < e; ++i) {
 			int a, b;
 			cin >> a >> b;
 			s[a].push_back(b);
 			s[b].push_back(a);
 		}
 
-		bool isNo = false;
+		bool isNotGraph = false;
 		vector<int> checkVertex(v + 1, -1);
-		for (int i = 1; i <= v; i++) //All vertex
-		{
+		
+		for (int i = 1; i <= v; ++i) {
 			if (checkVertex[i] != -1)
 				continue;
 			checkVertex[i] = i % 2;
             
 			queue<pair<int, int>> q;
 			q.push({ i, i % 2 });
-			while (!q.empty())
-			{
-				if (isNo)
+
+			while (!q.empty()) {
+				if (isNotGraph)
 					break;
 
 				pair<int, int> p = q.front(); q.pop();
 				int t = p.first;
 				int color = p.second;
 
-				for (int j = 0; j < s[t].size(); j++)
-				{
-					if (checkVertex[s[t][j]] == -1)
-					{
+				for (int j = 0; j < s[t].size(); ++j) {
+					if (checkVertex[s[t][j]] == -1) {
 						q.push({ s[t][j], (color + 1) % 2 });
 						checkVertex[s[t][j]] = (color + 1) % 2;
 					}
-					else if (checkVertex[s[t][j]] == color)
-					{
-						isNo = true;
+					else if (checkVertex[s[t][j]] == color) {
+						isNotGraph = true;
 						break;
 					}
 				}
 			}
 
-			if (isNo)
-			{
+			if (isNotGraph) {
 				cout << "NO\n";
 				break;
 			}
 		}
 
-		if (!isNo)
+		if (!isNotGraph)
 			cout << "YES\n";
 	}
 }
