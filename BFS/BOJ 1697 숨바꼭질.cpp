bcf61f14fa9ba8f91e716624a003cc652374683d
JAlthea <althea_@naver.com>
2022-01-08 15:00:00 +0900

    Modify code style

--- BFS/BOJ 1697 숨바꼭질.cpp
+++ BFS/BOJ 1697 숨바꼭질.cpp
@@ -1,27 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
+    int check[100001] = { 0 };
     int n, k;
     cin >> n >> k;
+
     queue<pair<int, int>> q;
     q.push({ 0, n });
-    int check[100001] = { 0 };
-    while (!q.empty())
-    {
+
+    while (!q.empty()) {
         pair<int, int> p = q.front(); q.pop();
-        if (p.second == k)
-        {
+
+        if (p.second == k) {
             cout << p.first;
             return 0;
         }
+
         if (check[p.second])
             continue;
-        check[p.second]++;
-        
+        ++check[p.second];
+
         int nx[] = { p.second + 1, p.second - 1, p.second * 2 };
-        for (int i=0; i<3; i++)
+
+        for (int i = 0; i < 3; ++i)
             if (nx[i] >= 0 && nx[i] <= 100000)
                 q.push({ p.first + 1, nx[i] });
     }
