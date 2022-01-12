baffa908d109f8507eb02b90cdd68ad52644a565
JAlthea <althea_@naver.com>
2022-01-13 05:02:02 +0900

    Modify code style

--- BFS/BOJ 2667 단지번호붙이기.cpp
+++ BFS/BOJ 2667 단지번호붙이기.cpp
@@ -1,32 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(pair<int, int> &a, pair<int, int> &b)
-{
+bool cmp(pair<int, int> &a, pair<int, int> &b) {
 	if (a.second == b.second)
 		return a.first < b.first;
 	return a.second < b.second;
 }
 
-map<int, int> bfs(vector<vector<char>> &board, vector<vector<int>> &home)
-{
+map<int, int> bfs(vector<vector<char>> &board, vector<vector<int>> &home) {
 	int dy[] = { -1, 1, 0, 0 };
 	int dx[] = { 0, 0, -1, 1 };
 	int n = board.size();
 	vector<vector<int>> check(n, vector<int>(n, 0));
 	map<int, int> m;
-	for (int i = 0; i < home.size(); i++)
-	{
+
+	for (int i = 0; i < home.size(); ++i) {
 		int y = home[i][0];
 		int x = home[i][1];
 		if (check[y][x])
 			continue;
 
 		queue<vector<int>> q;
-		q.push({ y, x, i + 1 });    //Color = 1, 2, ...
+		q.push({ y, x, i + 1 });    // Color = 1, 2, ...
 		m.insert({ i + 1, 0 });
-		while (!q.empty())
-		{
+
+		while (!q.empty()) {
 			vector<int> v = q.front(); q.pop();
 			y = v[0];
 			x = v[1];
@@ -34,18 +32,17 @@ map<int, int> bfs(vector<vector<char>> &board, vector<vector<int>> &home)
 
 			if (check[y][x])
 				continue;
-			check[y][x] = id;    //Coloring
-			m[id]++;
+			check[y][x] = id;    // Coloring
+			++m[id];
 
-			for (int i = 0; i < 4; i++)
-			{
+			for (int i = 0; i < 4; ++i) {
 				int ny = y + dy[i];
 				int nx = x + dx[i];
 				if (ny < 0 || nx < 0 || ny >= n || nx >= n)
 					continue;
 
 				if (board[ny][nx] == '1')
-					q.push({ ny, nx, id });    //Same Coloring
+					q.push({ ny, nx, id });    // Same Coloring
 			}
 		}
 	}
@@ -53,17 +50,16 @@ map<int, int> bfs(vector<vector<char>> &board, vector<vector<int>> &home)
 	return m;
 }
 
-int main()
-{
+int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int n;
 	cin >> n;
+	
 	vector<vector<char>> board(n, vector<char>(n, 0));
 	vector<vector<int>> home;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
 			cin >> board[i][j];
 			if (board[i][j] == '1')
 				home.push_back({ i, j, 0 });
@@ -74,6 +70,6 @@ int main()
 	vector<pair<int, int>> v(m.begin(), m.end());
 	sort(v.begin(), v.end(), cmp);
 	cout << v.size() << "\n";
-	for (auto i : v)
+	for (auto &i : v)
 		cout << i.second << " ";
 }
