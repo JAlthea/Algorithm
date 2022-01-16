8e3e134f23b71c1030c9b12310faa350e317378e
JAlthea <althea_@naver.com>
2022-01-17 01:21:30 +0900

    Modify code style

--- BFS/BOJ 11724 연결 요소의 개수.cpp
+++ BFS/BOJ 11724 연결 요소의 개수.cpp
@@ -5,9 +5,9 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n, m, s, t;
     cin >> n >> m;
-    
-    vector<int> d[1000000];
+
     vector<bool> b(n + 1);
+    vector<int> d[1000000];
     for (int i = 0; i < m; ++i) {
         cin >> s >> t;
         d[s].push_back(t);
@@ -18,18 +18,20 @@ int main() {
     for (int s = 1; s <= n; ++s) {
         if (b[s])
             continue;
-        
         b[s] = true;
+
         queue<int> q;
         q.push(s);
+        
         while (!q.empty()) {
             int now = q.front(); q.pop();
             for (int i = 0; i < d[now].size(); ++i) {
                 int e = d[now][i];
+
                 if (b[e])
                     continue;
-                
                 b[e] = true;
+
                 q.push(e);
             }
         }
