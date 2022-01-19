df1f091d0a372bcf78881e0c8d2d7a32c7a80db6
JAlthea <althea_@naver.com>
2022-01-19 15:22:30 +0900

    Modify code style

--- BFS/BOJ 13460 구슬 탈출 2.cpp
+++ BFS/BOJ 13460 구슬 탈출 2.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int n, m;
 	cin >> n >> m;
 	cin.ignore();
+
 	vector<vector<char>> board(n, vector<char>(m));
 	pair<int, int> redBall;
 	pair<int, int> blueBall;
 	pair<int, int> goal;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
 			cin >> board[i][j];
 			if (board[i][j] == 'R')
 				redBall = { i, j };
@@ -30,8 +28,8 @@ int main()
 	const int dx[] = { 1, -1, 0, 0 };
 	queue<vector<int>> q;
 	q.push({ 0, redBall.first, redBall.second, blueBall.first, blueBall.second });
-	while (!q.empty())
-	{
+
+	while (!q.empty()) {
 		vector<int> v = q.front(); q.pop();
 		int count = v[0];
 		int ry = v[1];
@@ -39,8 +37,7 @@ int main()
 		int by = v[3];
 		int bx = v[4];
 
-		if (count > 9)
-		{
+		if (count > 9) {
 			cout << -1;
 			return 0;
 		}
@@ -49,40 +46,35 @@ int main()
 			continue;
 		s.insert({ ry, rx, by, bx });
 
-		//4방향을 기준으로 goal을 만나는지 확인
-		for (int i = 0; i < 4; i++)
-		{
+		// 4방향을 기준으로 goal을 만나는지 확인
+		for (int i = 0; i < 4; ++i) {
 			int nry = ry;
 			int nrx = rx;
 			int nby = by;
 			int nbx = bx;
 
-			//빨간 공과 파란 공을 동시에 고려한 시뮬레이션
+			// 빨간 공과 파란 공을 동시에 고려한 시뮬레이션
 			bool isFailed = false;
-			while (1)
-			{
-				if (board[nry][nrx] == 'O')    //빨간 공 탈출 조건 (성공)
+			while (true) {
+				if (board[nry][nrx] == 'O')    // 빨간 공 탈출 조건 (성공)
 					break;
 
-				if (board[nby][nbx] == 'O')    //파란 공 탈출 조건 (실패)
-				{
+				if (board[nby][nbx] == 'O') {    // 파란 공 탈출 조건 (실패)
 					isFailed = true;
 					break;
 				}
 
-				//탈출 조건 : 둘다 움직이지 않을 때
+				// 탈출 조건 : 둘다 움직이지 않을 때
 				bool isEscapeA = true;
 				bool isEscapeB = true;
 
 				nry += dy[i];
 				nrx += dx[i];
-				if (nry == nby && nrx == nbx)    //빨간 공이 파란 공과 만날 때
-				{
+				if (nry == nby && nrx == nbx) {    // 빨간 공이 파란 공과 만날 때
 					nry -= dy[i];
 					nrx -= dx[i];
 				}
-				else if (board[nry][nrx] == '#')
-				{
+				else if (board[nry][nrx] == '#') {
 					nry -= dy[i];
 					nrx -= dx[i];
 				}
@@ -91,13 +83,11 @@ int main()
 				
 				nby += dy[i];
 				nbx += dx[i];
-				if (nby == nry && nbx == nrx)    //파란 공이 빨간 공과 만날 때
-				{
+				if (nby == nry && nbx == nrx) {    // 파란 공이 빨간 공과 만날 때
 					nby -= dy[i];
 					nbx -= dx[i];
 				}
-				else if (board[nby][nbx] == '#')
-				{
+				else if (board[nby][nbx] == '#') {
 					nby -= dy[i];
 					nbx -= dx[i];
 				}
@@ -108,15 +98,14 @@ int main()
 					break;
 			}
 
-			while (board[nby][nbx] != '#' && (nby != nry || nbx != nrx))	//빨간 공과 파란 공이 동시에 탈출하는 경우인지
-			{
+			while (board[nby][nbx] != '#' && (nby != nry || nbx != nrx)) {    // 빨간 공과 파란 공이 동시에 탈출하는 경우인지
 				nby += dy[i];
 				nbx += dx[i];
 				if (board[nby][nbx] == 'O')
 					isFailed = true;
 			}
-			if (board[nby][nbx] == '#' || (nby == nry && nbx == nrx))
-			{
+
+			if (board[nby][nbx] == '#' || (nby == nry && nbx == nrx)) {
 				nby -= dy[i];
 				nbx -= dx[i];
 			}
@@ -124,8 +113,7 @@ int main()
 			if (isFailed)
 				continue;
 
-			if (board[nry][nrx] == 'O')		//성공 조건
-			{
+			if (board[nry][nrx] == 'O')	{    // 성공 조건
 				cout << count + 1;
 				return 0;
 			}
