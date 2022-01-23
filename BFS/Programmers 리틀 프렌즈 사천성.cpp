548fef0e13ebc27e2a5f8563c4490baf4c965fe4
JAlthea <althea_@naver.com>
2022-01-23 17:34:30 +0900

    Modify code style

--- BFS/Programmers 리틀 프렌즈 사천성.cpp
+++ BFS/Programmers 리틀 프렌즈 사천성.cpp
@@ -14,11 +14,13 @@ string solution(int m, int n, vector<string> board) {
     vector<vector<pair<int, int>>> tiles(26);
     for (int i = 0; i < m; ++i)
         for (int j = 0; j < n; ++j)
-            if (isalpha(board[i][j])) tiles[board[i][j] - 'A'].push_back({ j, i });
+            if (isalpha(board[i][j])) 
+                tiles[board[i][j] - 'A'].push_back({ j, i });
 
     string answer = "";
     for (int i = 0; i < 26; ++i) {
-        if (tiles[i].empty()) continue;
+        if (tiles[i].empty()) 
+            continue;
 
         char startAlpha = i + 'A';
         int startX = tiles[i][0].first;
@@ -27,6 +29,7 @@ string solution(int m, int n, vector<string> board) {
         int goalY = tiles[i][1].second;
         queue<pair<pair<int, int>, Direction>> q;
         q.push({ tiles[i][0], { 0, 0, 0, 0, 0 } });
+
         while (!q.empty()) {
             pair<int, int> p = q.front().first;
             Direction d = q.front().second;
@@ -35,7 +38,8 @@ string solution(int m, int n, vector<string> board) {
             int y = p.second;
             char alpha = board[p.second][p.first];
 
-            if (d.count > 2) continue;
+            if (d.count > 2) 
+                continue;
 
             if ((goalX == x && goalY == y) && startAlpha == alpha) {
                 board[y][x] = '.';
@@ -46,26 +50,34 @@ string solution(int m, int n, vector<string> board) {
                 break;
             }
 
-            if (board[y][x] != '.' && board[y][x] != startAlpha) continue;
+            if (board[y][x] != '.' && board[y][x] != startAlpha) 
+                continue;
 
             if (d.left == 0 && x < n - 1) {
                 Direction nextD = { 0, 0, 0, 1, d.count };
-                if (!d.right) ++nextD.count;
+                if (!d.right) 
+                    ++nextD.count;
                 q.push({ { x + 1, y }, nextD });
             }
+
             if (d.right == 0 && x > 0) {
                 Direction nextD = { 0, 0, 1, 0, d.count };
-                if (!d.left) ++nextD.count;
+                if (!d.left) 
+                    ++nextD.count;
                 q.push({ { x - 1, y }, nextD }); 
-            }           
+            }
+
             if (d.bottom == 0 && y > 0) {
                 Direction nextD = { 1, 0, 0, 0, d.count };
-                if (!d.top) ++nextD.count;
+                if (!d.top) 
+                    ++nextD.count;
                 q.push({ { x, y - 1 }, nextD });
             }
+
             if (d.top == 0 && y < m - 1) {
                 Direction nextD = { 0, 1, 0, 0, d.count };
-                if (!d.bottom) ++nextD.count;
+                if (!d.bottom) 
+                    ++nextD.count;
                 q.push({ { x, y + 1 }, nextD });
             }
         }
