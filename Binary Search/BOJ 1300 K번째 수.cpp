478871768254f7926bd7d0c90eadcd8bd774343e
JAlthea <althea_@naver.com>
2022-01-26 00:10:30 +0900

    Modify code style

--- Binary Search/BOJ 1300 K번째 수.cpp
+++ Binary Search/BOJ 1300 K번째 수.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
 
-int main()
-{
+int main() {
     int n, k;
     scanf("%d%d", &n, &k);
     int left = 1;
     int right = k;
     int mid;
     int answer;
-    while (left <= right)
-    {
-	mid = (left + right) / 2;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+
         long long cnt = 0;
-	for (int i = 1; i <= n; ++i)
+        for (int i = 1; i <= n; ++i)
             cnt += std::min(mid / i, n);
         
-	if (cnt < k)
+    	if (cnt < k) {
             left = mid + 1;
-	else
-        {
-            answer = mid;
-            right = mid - 1;
+            continue;
         }
+    	
+        answer = mid;
+        right = mid - 1;
     }
+    
     printf("%d", answer);
 }
