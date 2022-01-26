1ae9afc6736121fb330373521803e2a0be1631de
JAlthea <althea_@naver.com>
2022-01-27 00:11:15 +0900

    Modify code style

--- Binary Search/BOJ 1477 휴게소 세우기.cpp
+++ Binary Search/BOJ 1477 휴게소 세우기.cpp
@@ -1,40 +1,39 @@
 #include <bits/stdc++.h>
 
-int main()
-{
+int main() {
 	int n, m, l;
 	scanf("%d%d%d", &n, &m, &l);
+	
 	std::vector<int> v;
-	for (int i = 0; i < n; ++i)
-	{
+	for (int i = 0; i < n; ++i) {
 		int tmp;
 		scanf("%d", &tmp);
 		v.push_back(tmp);
 	}
 	v.push_back(0);
 	v.push_back(l);
+
 	std::sort(v.begin(), v.end());
 
 	int max, min, mid, result;
 	max = l;
 	min = 0;
 	result = l;
-	while (min <= max) 
-	{
+	while (min <= max) {
 		mid = (min + max) / 2;
 		
 		int count = 0;
 		for (int i = 1; i < v.size(); ++i)
 			count += (v[i] - v[i - 1] - 1) / mid;
 
-		if (count > m)
+		if (count > m) {
 			min = mid + 1;
-		else
-		{
-			if (result > mid)
-				result = mid;
-			max = mid - 1;
+			continue;
 		}
+
+		if (result > mid)
+			result = mid;
+		max = mid - 1;
 	}
 
 	printf("%d", result);
