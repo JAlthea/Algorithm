b0655e04d32e71bc77b42a69ee5b22b03fc0888a
JAlthea <althea_@naver.com>
2022-01-28 00:09:15 +0900

    Modify code style

--- Binary Search/BOJ 1654 랜선 자르기.cpp
+++ Binary Search/BOJ 1654 랜선 자르기.cpp
@@ -2,31 +2,32 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
+int main() {
 	vector<long long> v;
 	long long sum = 0 , i, k, n, max, min, mid, c, res = 0;
 	scanf("%lld%lld", &k, &n);
 	v.resize(k);
 	for (i = 0; i < v.size(); sum += v[i], ++i)
 		scanf("%lld", &v[i]);
+
 	max = sum / n;
 	min = 1;
-	while (min <= max)
-	{
+	while (min <= max) {
 		mid = (max + min) / 2;
+
 		c = 0;
 		for (i = 0; i < v.size(); ++i)
 			c += v[i] / mid;
 
-		if (c >= n)
-		{
+		if (c >= n) {
 			if (res < mid)
 				res = mid;
 			min = mid + 1;
+			continue;
 		}
-		else if (c < n)
-			max = mid - 1;
+
+		max = mid - 1;
 	}
+
 	printf("%lld", res);
 }
