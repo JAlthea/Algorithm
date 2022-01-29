fd35be9d6d2f33802e193454ec3b9636db408f05
JAlthea <althea_@naver.com>
2022-01-30 00:09:31 +0900

    Modify code style

--- Binary Search/BOJ 1920 수 찾기.cpp
+++ Binary Search/BOJ 1920 수 찾기.cpp
@@ -2,10 +2,14 @@
 #include <algorithm>
 
 int n, i, t, d[100001];
-int main()
-{
-	for(scanf("%d", &n); i<n; ++i) scanf("%d", &d[i]);
+
+int main() {
+	for (scanf("%d", &n); i < n; ++i) 
+		scanf("%d", &d[i]);
+	
 	std::sort(d, d + n);
+	
 	scanf("%d", &i);
-	while(i--) scanf("%d", &t), printf("%d\n", std::binary_search(d, d + n, t));
+	while (i--) 
+		scanf("%d", &t), printf("%d\n", std::binary_search(d, d + n, t));
 }
