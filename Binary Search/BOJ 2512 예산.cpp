3e2964e32e178e4c149ac4efdccbe44b8ee14814
JAlthea <althea_@naver.com>
2022-02-03 00:01:54 +0900

    Modify code style

--- Binary Search/BOJ 2512 예산.cpp
+++ Binary Search/BOJ 2512 예산.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-        ios::sync_with_stdio(0); cin.tie(0);
-	int n, m;
-	cin >> n;
-	vector<int> v(n);
-	for (int i = 0; i < n; ++i)
-		cin >> v[i];
-	cin >> m;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+    cin >> m;
 
-	int min = 0, max = *max_element(v.begin(), v.end()), mid;
-	while (min <= max)
-	{
-		mid = (min + max) / 2;
-		int count = 0;
-		for (int i = 0; i < n; ++i)
-                        count += v[i] > mid ? mid : v[i];
+    int mid, min = 0, max = *max_element(v.begin(), v.end());
+    while (min <= max) {
+        mid = (min + max) / 2;
 
-		if (count <= m)
-			min = mid + 1;
-		else
-			max = mid - 1;
-	}
+        int count = 0;
+        for (int i = 0; i < n; ++i)
+            count += v[i] > mid ? mid : v[i];
 
-	printf("%d", max);
+        if (count <= m) {
+            min = mid + 1;
+            continue;
+        }
+        
+        max = mid - 1;
+    }
+
+    printf("%d", max);
 }
