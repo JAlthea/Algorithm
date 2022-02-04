c53da74206e58f3a1485d6e323f9cf0a4c28ecda
JAlthea <althea_@naver.com>
2022-02-05 00:03:55 +0900

    Modify code style

--- Binary Search/BOJ 3079 입국심사.cpp
+++ Binary Search/BOJ 3079 입국심사.cpp
@@ -1,36 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     long long n, m;
     cin >> n >> m;
     vector<long long> times(n);
-    for (int i=0; i<n; i++)
+    for (int i = 0; i < n; ++i)
         cin >> times[i];
     
     long long answer = LLONG_MAX;
     long long s = *min_element(times.begin(), times.end()) * m / times.size();
     long long e = *max_element(times.begin(), times.end()) * m / times.size();
     long long mid;
-    while (s <= e)
-    {
+
+    while (s <= e) {
         mid = (s + e) / 2;
+
         long long tmp = 0;
-        for (int i=0; i<n; i++)
+        for (int i = 0; i < n; ++i)
             tmp += mid / times[i];
         
-        if (tmp < m)
-        {
+        if (tmp < m) {
             s = mid + 1;
+            continue;
         }
-        else
-        {
-            if (answer > mid)
-                answer = mid;
-            e = mid - 1;
-        }
+
+        if (answer > mid)
+            answer = mid;
+        e = mid - 1;
     }
     
     cout << answer;
