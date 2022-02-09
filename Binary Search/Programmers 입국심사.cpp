1b074b1f25791362c6908b5dc0de91e5f4b6cd13
JAlthea <althea_@naver.com>
2022-02-10 00:00:00 +0900

    Modify code style

--- Binary Search/Programmers 입국심사.cpp
+++ Binary Search/Programmers 입국심사.cpp
@@ -6,20 +6,22 @@ long long solution(int n, vector<int> times) {
     long long s = *min_element(times.begin(), times.end()) * n / times.size();
     long long e = *max_element(times.begin(), times.end()) * n / times.size();
     long long m;
+
     while (s <= e) {
         m = (s + e) / 2;
+        
         long long tmp = 0;
         for (int i = 0; i < times.size(); ++i)
             tmp += m / times[i];
 
         if (tmp < n) {
             s = m + 1;
+            continue;
         }
-        else {
-            if (answer > m)
-                answer = m;
-            e = m - 1;
-        }
+        
+        if (answer > m)
+            answer = m;
+        e = m - 1;
     }
 
     return answer;
