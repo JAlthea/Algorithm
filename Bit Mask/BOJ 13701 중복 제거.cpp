c64043cb02c61848a7c2358ec0fe2f30a8070083
Yongmin Jang <althea_@naver.com>
2022-02-14 02:57:55 +0900

    Modify code style

--- Bit Mask/BOJ 13701 중복 제거.cpp
+++ Bit Mask/BOJ 13701 중복 제거.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     vector<bool> d(1 << 25);
+    int t;
+    
     while (!cin.eof()) {
-        int t;
         cin >> t;
         if (!d[t]) 
           cout << t << ' ';
