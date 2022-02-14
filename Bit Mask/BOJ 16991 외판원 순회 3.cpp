f7986b0035b0d264f707536c749abf2e142fa5e7
Yongmin Jang <althea_@naver.com>
2022-02-15 02:41:22 +0900

    Modify code style

--- Bit Mask/BOJ 16991 외판원 순회 3.cpp
+++ Bit Mask/BOJ 16991 외판원 순회 3.cpp
@@ -4,22 +4,25 @@ using namespace std;
 int n;
 double cache[16][65536], dist[16][16];
 
-double TSP(int from, int visited)
-{
-	if (visited == (1 << n) - 1) return dist[from][0];
-	double &ret = cache[from][visited];
-	if (ret) return ret;
+double TSP(int from, int visited) {
+	if (visited == (1 << n) - 1) 
+		return dist[from][0];
+	
+	ouble &ret = cache[from][visited];
+	if (ret) 
+		return ret;
 	ret = 1e8;
-	for (int to = 0; to < n; ++to)
-	{
-		if (visited & (1 << to)) continue;
+	
+	for (int to = 0; to < n; ++to) {
+		if (visited & (1 << to)) 
+			continue;
 		ret = min(ret, TSP(to, visited | (1 << to)) + dist[from][to]);
 	}
+	
 	return ret;
 }
 
-int main()
-{
+int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int N;
 	cin >> N;
