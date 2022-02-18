392646d4755baebb46e76caa1d52e01b38610c34
Yongmin Jang <althea_@naver.com>
2022-02-19 01:05:50 +0900

    Modify code style

--- Brute Force/BOJ 12100 2048 (Easy).cpp
+++ Brute Force/BOJ 12100 2048 (Easy).cpp
@@ -7,46 +7,39 @@ const int LEFT = 3;
 const int RIGHT = 4;
 int maxBlock;
 
-int checkBoard(vector<vector<int>> &board)
-{
+int checkBoard(vector<vector<int>> &board) {
 	int value = 0;
-	for (int i = 0; i < board.size(); i++)
-		for (int j = 0; j < board.size(); j++)
+	for (int i = 0; i < board.size(); ++i)
+		for (int j = 0; j < board.size(); ++j)
 			value = max(value, board[i][j]);
 	return value;
 }
 
-vector<vector<int>> collide(vector<vector<int>> &board, int direction)
-{
+vector<vector<int>> collide(vector<vector<int>> &board, int direction) {
 	int n = board.size();
 	vector<vector<int>> newBoard = board;
 	vector<vector<int>> saveBlock(n, vector<int>(n, 0));
-	if (direction == UP)
-	{
-		//part1. 위부터 아래로 검사해서 같은 라인에 있는게 있으면 우선적으로 합체
-		for (int j = 0; j < n; j++)
-		{
+	
+	if (direction == UP) {
+		// Part1. 위부터 아래로 검사해서 같은 라인에 있는게 있으면 우선적으로 합체
+		for (int j = 0; j < n; ++j) {
 			int p1 = 0, p2 = 0;
 			int index = 0;
-			for (int i = 0; i < n; i++)
-			{
-				if (newBoard[i][j])
-				{
+			for (int i = 0; i < n; ++i) {
+				if (newBoard[i][j]) {
 					if (p1 == 0)
 						p1 = newBoard[i][j];
 					else if (p2 == 0)
 						p2 = newBoard[i][j];
 				}
 
-				if (p1 && p2)    //block checking
-				{
-					if (p1 == p2)
-					{
+				// Block checking
+				if (p1 && p2) {
+					if (p1 == p2) {
 						saveBlock[index++][j] = p1 + p2;
 						p1 = 0; p2 = 0;
 					}
-					else
-					{
+					else {
 						saveBlock[index++][j] = p1;
 						p1 = p2; p2 = 0;
 					}
@@ -58,32 +51,26 @@ vector<vector<int>> collide(vector<vector<int>> &board, int direction)
 
 		newBoard = saveBlock;
 	}
-	else if (direction == DOWN)
-	{
-		//part1. 아래부터 위로 검사해서 같은 라인에 있는게 있으면 우선적으로 합체
-		for (int j = 0; j < n; j++)
-		{
+	else if (direction == DOWN) {
+		// Part1. 아래부터 위로 검사해서 같은 라인에 있는게 있으면 우선적으로 합체
+		for (int j = 0; j < n; ++j) {
 			int p1 = 0, p2 = 0;
 			int index = n - 1;
-			for (int i = n - 1; i >= 0; i--)
-			{
-				if (newBoard[i][j])
-				{
+			for (int i = n - 1; i >= 0; --i) {
+				if (newBoard[i][j]) {
 					if (p1 == 0)
 						p1 = newBoard[i][j];
 					else if (p2 == 0)
 						p2 = newBoard[i][j];
 				}
 
-				if (p1 && p2)    //block checking
-				{
-					if (p1 == p2)
-					{
+				// Block checking
+				if (p1 && p2) {
+					if (p1 == p2) {
 						saveBlock[index--][j] = p1 + p2;
 						p1 = 0; p2 = 0;
 					}
-					else
-					{
+					else {
 						saveBlock[index--][j] = p1;
 						p1 = p2; p2 = 0;
 					}
@@ -95,32 +82,26 @@ vector<vector<int>> collide(vector<vector<int>> &board, int direction)
 
 		newBoard = saveBlock;
 	}
-	else if (direction == LEFT)
-	{
-		//part1. 왼쪽부터 오른쪽으로 검사해서 같은 라인에 있는게 있으면 우선적으로 합체
-		for (int i = 0; i < n; i++)
-		{
+	else if (direction == LEFT) {
+		// Part1. 왼쪽부터 오른쪽으로 검사해서 같은 라인에 있는게 있으면 우선적으로 합체
+		for (int i = 0; i < n; ++i) {
 			int p1 = 0, p2 = 0;
 			int index = 0;
-			for (int j = 0; j < n; j++)
-			{
-				if (newBoard[i][j])
-				{
+			for (int j = 0; j < n; ++j) {
+				if (newBoard[i][j]) {
 					if (p1 == 0)
 						p1 = newBoard[i][j];
 					else if (p2 == 0)
 						p2 = newBoard[i][j];
 				}
 
-				if (p1 && p2)    //block checking
-				{
-					if (p1 == p2)
-					{
+				// Block checking
+				if (p1 && p2) {
+					if (p1 == p2) {
 						saveBlock[i][index++] = p1 + p2;
 						p1 = 0; p2 = 0;
 					}
-					else
-					{
+					else {
 						saveBlock[i][index++] = p1;
 						p1 = p2; p2 = 0;
 					}
@@ -132,32 +113,26 @@ vector<vector<int>> collide(vector<vector<int>> &board, int direction)
 
 		newBoard = saveBlock;
 	}
-	else if (direction == RIGHT)
-	{
-		//part1. 오른쪽부터 왼쪽으로 검사해서 같은 라인에 있는게 있으면 우선적으로 합체
-		for (int i = 0; i < n; i++)
-		{
+	else if (direction == RIGHT) {
+		// Part1. 오른쪽부터 왼쪽으로 검사해서 같은 라인에 있는게 있으면 우선적으로 합체
+		for (int i = 0; i < n; ++i) {
 			int p1 = 0, p2 = 0;
 			int index = n - 1;
-			for (int j = n - 1; j >= 0; j--)
-			{
-				if (newBoard[i][j])
-				{
+			for (int j = n - 1; j >= 0; --j) {
+				if (newBoard[i][j]) {
 					if (p1 == 0)
 						p1 = newBoard[i][j];
 					else if (p2 == 0)
 						p2 = newBoard[i][j];
 				}
 
-				if (p1 && p2)    //block checking
-				{
-					if (p1 == p2)
-					{
+				// Block checking
+				if (p1 && p2) {
+					if (p1 == p2) {
 						saveBlock[i][index--] = p1 + p2;
 						p1 = 0; p2 = 0;
 					}
-					else
-					{
+					else {
 						saveBlock[i][index--] = p1;
 						p1 = p2; p2 = 0;
 					}
@@ -173,10 +148,8 @@ vector<vector<int>> collide(vector<vector<int>> &board, int direction)
 	return newBoard;
 }
 
-void backTracking(vector<vector<int>> &board, int depth)
-{
-	if (depth == 5)
-	{
+void backTracking(vector<vector<int>> &board, int depth) {
+	if (depth == 5) {
 		maxBlock = max(maxBlock, checkBoard(board));
 		return;
 	}
@@ -196,13 +169,12 @@ void backTracking(vector<vector<int>> &board, int depth)
 	backTracking(newBoard, depth + 1);
 }
 
-int main()
-{
+int main() {
 	int n;
 	cin >> n;
 	vector<vector<int>> board(n, vector<int>(n, 0));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
 			cin >> board[i][j];
 	backTracking(board, 0);
 	cout << maxBlock;
