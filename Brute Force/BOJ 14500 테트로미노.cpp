550c71bfc288f9c983a1d153e9beb261f13aa278
Yongmin Jang <althea_@naver.com>
2022-02-21 10:38:35 +0900

    Modify code style

--- Brute Force/BOJ 14500 테트로미노.cpp
+++ Brute Force/BOJ 14500 테트로미노.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> rotateBoard(vector<vector<int>> &board, int n, int m)
-{
+vector<vector<int>> rotateBoard(vector<vector<int>> &board, int n, int m) {
 	vector<vector<int>> rotatedBoard(m, vector<int>(n, 0));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
 			rotatedBoard[j][n - 1 - i] = board[i][j];
 	return rotatedBoard;
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 	int n, m;
 	cin >> n >> m;
 	vector<vector<int>> board(n, vector<int>(m, 0));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
 			cin >> board[i][j];
 
 	vector<pair<int, int>> 
@@ -39,22 +37,18 @@ int main()
 	tetro.push_back(O);
 
 	int maxSum = 0;
-	for (int k = 0; k < 4; k++)	//rotate
-	{
+	for (int k = 0; k < 4; ++k) { // Rotate
 		int N = board.size();
 		int M = board[0].size();
 
-		for (int x = 0; x < 7; x++)	//figure
-		{
+		for (int x = 0; x < 7; ++x) {    // Figure
 			vector<pair<int, int>> fig = tetro[x];
-			for (int i = 0; i < N; i++)
-			{
-				for (int j = 0; j < M; j++)
-				{
-					//i, j 방향으로 이동하면서, fig블록 4개의 합을 최신화한다.
+			
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < M; ++j) {
+					// i, j 방향으로 이동하면서, fig블록 4개의 합을 최신화한다.
 					int nowSum = 0;
-					for (int c = 0; c < 4; c++)
-					{
+					for (int c = 0; c < 4; ++c) {
 						if (fig[c].first + i > N - 1 || fig[c].second + j > M - 1)
 							break;
 						nowSum += board[fig[c].first + i][fig[c].second + j];
