1e26e852842e2bb3ce61a59b63fec7a2c93aec9c
Yongmin Jang <althea_@naver.com>
2022-02-23 02:29:12 +0900

    Modify code style

--- Brute Force/BOJ 2798 블랙잭.cpp
+++ Brute Force/BOJ 2798 블랙잭.cpp
@@ -1,28 +1,24 @@
 #import <ios>
 
-int main()
-{
+int main() {
 	int n, m, d[99];
 	scanf("%d%d", &n, &m);
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; ++i)
 		scanf("%d", &d[i]);
 
 	int sum = m;
 	int min = 999999;
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; ++i) {
 		if (m - d[i] < 2)
 			continue;
 
 		int a = sum - d[i];
-		for (int j = i + 1; j < n; j++)
-		{
+		for (int j = i + 1; j < n; ++j) {
 			if (a - d[j] < 1)
 				continue;
 			
 			int b = a - d[j];
-			for (int k = j + 1; k < n; k++)
-			{
+			for (int k = j + 1; k < n; ++k) {
 				if (b - d[k] < 0)
 					continue;
 
