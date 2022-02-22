c7980642cd8d2ff5c4d624023f305cf5872d7708
Yongmin Jang <althea_@naver.com>
2022-02-22 16:05:52 +0900

    Modify code style
    simple test?

--- Brute Force/BOJ 2231 분해합.cpp
+++ Brute Force/BOJ 2231 분해합.cpp
@@ -1,17 +1,19 @@
 #import <ios>
+
 int n, i, a, b;
-int main()
-{
-    for (scanf("%d", &n); i<n; i++)
-    {
+
+int main() {
+    for (scanf("%d", &n); i < n; ++i) {
         a = b = i;
-        while (b)
+        
+        while (b != 0)
             a += b % 10, b /= 10;
-        if (a == n)
-        {
+        
+        if (a == n) {
             printf("%d", i);
             return 0;
         }
     }
+    
     puts("0");
 }
