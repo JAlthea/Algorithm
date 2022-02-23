de2cf99ad7dd0658403210fec16f7c88a318ac4f
Yongmin Jang <althea_@naver.com>
2022-02-24 00:15:28 +0900

    Modify code style

--- Brute Force/BOJ 7568 덩치.cpp
+++ Brute Force/BOJ 7568 덩치.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int n, d[51][2], c[51];
-int main()
-{
+
+int main() {
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; ++i)
 		scanf("%d%d", &d[i][0], &d[i][1]);
 
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; ++i) {
 		int count = 0;
-		for (int j = 0; j < n; j++)
-		{
+		for (int j = 0; j < n; ++j) {
 			if (i == j)
 				continue;
+			
 			if (d[i][0] < d[j][0] && d[i][1] < d[j][1])
-				count++;
+				++count;
 		}
+		
 		c[i] = count + 1;
 	}
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; ++i)
 		printf("%d ", c[i]);
 }
