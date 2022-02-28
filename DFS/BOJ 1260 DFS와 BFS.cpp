6d4683a77dc8840f31f5f3cd6e49e61df43689d1
Yongmin Jang <althea_@naver.com>
2022-02-28 21:13:42 +0900

    Modify code style

--- DFS/BOJ 1260 DFS와 BFS.cpp
+++ DFS/BOJ 1260 DFS와 BFS.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int n, m, start;
-void dfs(vector<vector<int>> &v, int start)
-{
+
+void dfs(vector<vector<int>> &v, int start) {
 	vector<bool> visited(n + 1, false);
 	stack<int> s;
 	s.push(start);
-	while (!s.empty())
-	{
-		int now = s.top();
-		s.pop();
+	
+	while (!s.empty()) {
+		int now = s.top(); s.pop();
+		
 		if (visited[now])
 			continue;
 		visited[now] = true;
@@ -22,15 +22,14 @@ void dfs(vector<vector<int>> &v, int start)
 	}
 }
 
-void bfs(vector<vector<int>> &v, int start)
-{
+void bfs(vector<vector<int>> &v, int start) {
 	vector<bool> visited(n + 1, false);
 	queue<int> q;
 	q.push(start);
-	while (!q.empty())
-	{
-		int now = q.front();
-		q.pop();
+	
+	while (!q.empty()) {
+		int now = q.front(); q.pop();
+		
 		if (visited[now])
 			continue;
 		visited[now] = true;
@@ -42,16 +41,16 @@ void bfs(vector<vector<int>> &v, int start)
 	}
 }
 
-int main()
-{
+int main() {
 	scanf("%d%d%d", &n, &m, &start);
 	vector<vector<int>> v(n + 1, vector<int>(n + 1, 0));
 	int a, b;
-	while (~scanf("%d%d", &a, &b))
-	{
-		v[a][b]++;
-		v[b][a]++;
+	
+	while (~scanf("%d%d", &a, &b)) {
+		++v[a][b];
+		++v[b][a];
 	}
+	
 	dfs(v, start);
 	puts("");
 	bfs(v, start);
