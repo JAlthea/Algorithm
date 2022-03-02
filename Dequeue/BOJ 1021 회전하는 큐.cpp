a1e5ec7f3612a3817e62f2e6606ee0c7c9560788
Yongmin Jang <althea_@naver.com>
2022-03-03 01:47:41 +0900

    Modify code style

--- Dequeue/BOJ 1021 회전하는 큐.cpp
+++ Dequeue/BOJ 1021 회전하는 큐.cpp
@@ -1,54 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findIndex(deque<int> &d, int n)
-{
+int findIndex(deque<int> &d, int n) {
     int index = 0;
-    deque<int>::iterator iter;
-    for(iter = d.begin(); iter != d.end(); ++iter, ++index)     
-        if(*iter == n)
+    for (deque<int>::iterator iter = d.begin(); iter != d.end(); ++iter, ++index)     
+        if (*iter == n)
             break;
+    
     return index;
 }
 
-int main()
-{
+int main() {
     int n, m;
     scanf("%d%d", &n, &m);
+    
     vector<int> v(m);
-    for(int i=0; i<m; ++i)
+    for (int i = 0; i < m; ++i)
         scanf("%d", &v[i]); 
+    
     deque<int> d;
-    for(int i=1; i<=n; +i)
+    for (int i = 1; i <= n; +i)
         d.push_back(i);
+    
         
     int sum = 0;
-    for(int i=0; i<m; ++i)
-    {
+    for (int i = 0; i < m; ++i) {
         int index = findIndex(d, v[i]);
         bool reverse = false;
-        if(index > d.size() - index)
-        {
+        
+        if (index > d.size() - index) {
             index = d.size() - index;
             reverse = true;
         }
         
         sum += index;
-        while(index--)
-        {
-            if(reverse)
-            {
+        while (index--) {
+            if (reverse) {
                 d.push_front(d.back());
                 d.pop_back();
             }
-            else
-            {
+            else {
                 d.push_back(d.front());
                 d.pop_front();
             }
         }
         
-        if(d.front() == v[i])
+        if (d.front() == v[i])
             d.pop_front();
     }
     
