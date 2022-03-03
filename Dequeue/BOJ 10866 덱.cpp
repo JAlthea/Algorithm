a4000a7d1f107e93e12b7453cf80f6ff026e4c2b
Yongmin Jang <althea_@naver.com>
2022-03-04 00:04:31 +0900

    Modify code style

--- Dequeue/BOJ 10866 덱.cpp
+++ Dequeue/BOJ 10866 덱.cpp
@@ -1,63 +1,53 @@
 #include <queue>
 #include <iostream>
-
 using namespace std;
-int main()
-{
+
+int main() {
     int n;
     scanf("%d", &n);
     deque<int> d;
-    while(n--)
-    {
+    
+    while (n--) {
         string tmp;
         int m;
         cin >> tmp;
-        if(tmp == "push_back")
-        {
+        
+        if (tmp == "push_back") {
             cin >> m;
             d.push_back(m);
         }
-        else if(tmp == "push_front")
-        {
+        else if (tmp == "push_front") {
             cin >> m;
             d.push_front(m);
         }
-        else if(tmp == "front")
-        {
-            if(d.empty())
-            {
+        else if (tmp == "front") {
+            if (d.empty()) {
                 cout << "-1" << '\n';
                 continue;
             }
             cout << d.front() << '\n';
         }
-        else if(tmp == "back")
-        {
-            if(d.empty())
-            {
+        else if (tmp == "back") {
+            if (d.empty()) {
                 cout << "-1" << '\n';
                 continue;
             }
             cout << d.back() << '\n';
         }
-        else if(tmp == "size")
+        else if (tmp == "size")
             cout << d.size() << '\n';
-        else if(tmp == "empty")
+        else if (tmp == "empty")
             cout << d.empty() << '\n';
-        else if(tmp == "pop_back")
-        {
-            if(d.empty())
-            {
+        else if (tmp == "pop_back") {
+            if (d.empty()) {
                 cout << "-1" << '\n';
                 continue;
             }
             cout << d.back() << '\n';
             d.pop_back();
         }
-        else if(tmp == "pop_front")
-        {
-            if(d.empty())
-            {
+        else if (tmp == "pop_front" ){
+            if (d.empty()) {
                 cout << "-1" << '\n';
                 continue;
             }
