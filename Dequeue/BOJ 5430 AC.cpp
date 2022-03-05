29c6c36e12ee0c0aefe6f1e57affbc22c096c3da
Yongmin Jang <althea_@naver.com>
2022-03-05 17:11:00 +0900

    Modify code style

--- Dequeue/BOJ 5430 AC.cpp
+++ Dequeue/BOJ 5430 AC.cpp
@@ -1,77 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	int T, n;
 	cin >> T;
-	while(T--)
-	{
+	
+	while (T--) {
 		string s, tmp;
 		cin >> s;
 		cin >> n;
 		cin >> tmp;
 		deque<int> d;
-		for(int i = 0; i < tmp.size(); ++i)
-		{
-			if(tmp[i] < 48 || tmp[i] > 57)
+		
+		for (int i = 0; i < tmp.size(); ++i) {
+			if (tmp[i] < 48 || tmp[i] > 57)
 				continue;
 
 			string now = "";
-			while(tmp[i] > 47 && tmp[i] < 58)
-			{
+			while (tmp[i] > 47 && tmp[i] < 58) {
 				now += tmp[i];
-				i++;
+				++i;
 			}
 			d.push_back(stoi(now));
 		}
 	
 		bool reverse = false;
 		bool check = false;
-		for(int i = 0; i < s.size(); ++i)
-		{
-			if(s[i] == 'R')
+		
+		for (int i = 0; i < s.size(); ++i) {
+			if (s[i] == 'R') {
 				reverse = !reverse;
-			else
-			{
-				if(d.empty())
-				{
-					cout << "error\n";
-					check = true;
-					break;
-				}
+				continue;
+			}
+			
+			if (d.empty()) {
+				cout << "error\n";
+				check = true;
+				break;
+			}
 
-				if(reverse)
-					d.pop_back();
-				else
-					d.pop_front();
+			if (reverse)
+				d.pop_back();
+			else
+				d.pop_front();
 			}
 		}
-		if(check)
+	
+		if (check)
 			continue;
 
 		int size = d.size();
 		cout << '[';
-		for(int i = 0; i < size; ++i)
-		{
-			if(reverse)
-			{
-				if(i == size - 1)
-					cout << d.back();
-				else
-					cout << d.back() << ',';
+		
+		for (int i = 0; i < size; ++i) {
+			if (reverse) {
+				cout << d.back();
+				if (i == size - 1)
+					cout << ',';
 				d.pop_back();
 			}
-			else
-			{
-				if(i == size - 1)
-					cout << d.front();
-				else
-					cout << d.front() << ',';
+			else {
+				cout << d.front();
+				if (i == size - 1) 
+					cout << ',';
 				d.pop_front();
 			}
 		}
+	
 		cout << "]\n";
 	}
 }
