85cda710b1b5a9e9e11a940919ac28cdda9a1951
Yongmin Jang <althea_@naver.com>
2022-03-10 00:54:05 +0900

    Modify code style

--- Dijkstra Algorithm/BOJ 1504 특정한 최단 경로.cpp
+++ Dijkstra Algorithm/BOJ 1504 특정한 최단 경로.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
 	int n, m, x, y;
 	scanf("%d%d", &n, &m);
-	vector<vector<pair<int, int>>> v(2*n + 1);
-	while (m--)
-	{
+	vector<vector<pair<int, int>>> v(2 * n + 1);
+	
+	while (m--) {
 		int a, b, c;
 		scanf("%d%d%d", &a, &b, &c);
 		v[a].push_back({ b, c });
@@ -16,34 +15,29 @@ int main()
 	scanf("%d%d", &x, &y);
 
 	int s[2] = { 0 };
-	for (int k = 0; k < 2; ++k)
-	{
+	for (int k = 0; k < 2; ++k) {
 		vector<int> d;
 		if (k == 0)
 			d = { 1, x, y, n };
 		else
 			d = { 1, y, x, n };
 
-		for (int i = 0; i < 3; ++i)
-		{
+		for (int i = 0; i < 3; ++i) {
 			vector<int> dist(n + 1, 999999);
 			priority_queue<pair<int, int>> pq;
 			pq.push({ 0, d[i] });
 			dist[d[i]] = 0;
 
-			while (!pq.empty())
-			{
+			while (!pq.empty()) {
 				int cost = -1 * pq.top().first;
 				int here = pq.top().second;
 				pq.pop();
 
-				for (int i = 0; i < v[here].size(); ++i)
-				{
+				for (int i = 0; i < v[here].size(); ++i) {
 					int next = v[here][i].first;
 					int nextCost = v[here][i].second;
 
-					if (dist[next] > dist[here] + nextCost)
-					{
+					if (dist[next] > dist[here] + nextCost) {
 						dist[next] = dist[here] + nextCost;
 						pq.push({ -1 * dist[next], next });
 					}
