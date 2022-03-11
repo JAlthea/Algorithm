f1ac9eb77bec2d6ca5ce61d6b400c4d1af992b24
Yongmin Jang <althea_@naver.com>
2022-03-12 02:38:07 +0900

    Modify code style

--- Dijkstra Algorithm/BOJ 9370 미확인 도착지.cpp
+++ Dijkstra Algorithm/BOJ 9370 미확인 도착지.cpp
@@ -1,33 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
 	int T;
 	scanf("%d", &T);
-	while (T--)
-	{
+	
+	while (T--) {
 		int n, m, t, s, g, h;
 		scanf("%d%d%d%d%d%d", &n, &m, &t, &s, &g, &h);
 		vector<vector<pair<int, int>>> v(2 * n + 1);
 		vector<int> ts(t);
-		while (m--)
-		{
+		
+		while (m--) {
 			int a, b, d;
 			scanf("%d%d%d", &a, &b, &d);
 			v[a].push_back({ b, d });
 			v[b].push_back({ a, d });
 		}
-		for (int i = 0; i < t; i++)
+		
+		for (int i = 0; i < t; ++i)
 			scanf("%d", &ts[i]);
 		
 		sort(ts.begin(), ts.end());
 
-		for (int q = 0; q < t; q++)
-		{
+		for (int q = 0; q < t; ++q) {
 			int tmp[3] = { 0 };
-			for (int k = 0; k < 3; k++)
-			{
+			
+			for (int k = 0; k < 3; ++k) {
 				vector<int> d;
 				if (k == 0)
 					d = { s, g, h, ts[q] };
@@ -36,26 +35,22 @@ int main()
 				else
 					d = { s, ts[q] };
 
-				for (int i = 0; i < d.size() - 1; i++)
-				{
+				for (int i = 0; i < d.size() - 1; ++i) {
 					vector<int> dist(n + 1, 999999999);
 					priority_queue<pair<int, int>> pq;
 					pq.push({ 0, d[i] });
 					dist[d[i]] = 0;
 
-					while (!pq.empty())
-					{
+					while (!pq.empty()) {
 						int cost = -1 * pq.top().first;
 						int here = pq.top().second;
 						pq.pop();
 
-						for (int i = 0; i < v[here].size(); i++)
-						{
+						for (int i = 0; i < v[here].size(); ++i) {
 							int next = v[here][i].first;
 							int nextCost = v[here][i].second;
 
-							if (dist[next] > dist[here] + nextCost)
-							{
+							if (dist[next] > dist[here] + nextCost) {
 								dist[next] = dist[here] + nextCost;
 								pq.push({ -1 * dist[next], next });
 							}
