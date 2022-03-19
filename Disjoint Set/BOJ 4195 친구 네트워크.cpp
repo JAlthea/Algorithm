05ba7d98ffe29277a2c2a6f6b5cd1ae9bbeaeabe
Yongmin Jang <althea_@naver.com>
2022-03-20 06:53:53 +0900

    Modify code style

--- Disjoint Set/BOJ 4195 친구 네트워크.cpp
+++ Disjoint Set/BOJ 4195 친구 네트워크.cpp
@@ -4,50 +4,55 @@ using namespace std;
 unordered_map<string, string> s;
 unordered_map<string, int> h;
 
-string findSet(string &e)
-{
-	if (s[e] == e) return e;
+string findSet(string &e) {
+	if (s[e] == e) 
+		return e;
+	
 	return s[e] = findSet(s[e]);
 }
 
-void unionSet(string &a, string &b)
-{
+void unionSet(string &a, string &b) {
 	a = findSet(a);
 	b = findSet(b);
-	if (a == b) return;
+	if (a == b) 
+		return;
 
-	if (h[a] < h[b])
-	{
+	if (h[a] < h[b]) {
 		h[b] += h[a];
 		s[a] = b;
 	}
-	else
-	{
+	else {
 		h[a] += h[b];
 		s[b] = a;
 	}
 }
 
-int main()
-{
+int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int t;
 	cin >> t;
-	while (t--)
-	{
+	
+	while (t--) {
 		s.clear();
 		h.clear();
 		int f;
 		string from, to;
 		cin >> f;
-		for (int i = 0; i < f; ++i)
-		{
+		
+		for (int i = 0; i < f; ++i) {
 			cin >> from >> to;
-			if (s.find(from) == s.end()) s[from] = from;
-			if (s.find(to) == s.end()) s[to] = to;
-			if (h.find(from) == h.end()) h[from] = 1;
-			if (h.find(to) == h.end()) h[to] = 1;
+			
+			if (s.find(from) == s.end()) 
+				s[from] = from;
+			if (s.find(to) == s.end()) 
+				s[to] = to;
+			if (h.find(from) == h.end()) 
+				h[from] = 1;
+			if (h.find(to) == h.end()) 
+				h[to] = 1;
+			
 			unionSet(from, to);
+			
 			cout << max(h[findSet(from)], h[findSet(to)]) << '\n';
 		}
 	}
