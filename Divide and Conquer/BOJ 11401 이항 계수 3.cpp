7274bbe3d714970e87c6090d9e263a9c3215979f
Yongmin Jang <althea_@naver.com>
2022-03-23 02:24:25 +0900

    Modify code style

--- Divide and Conquer/BOJ 11401 이항 계수 3.cpp
+++ Divide and Conquer/BOJ 11401 이항 계수 3.cpp
@@ -1,54 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-const long long p = 1000000007;    //prime nubmer, MOD
 
-long long pow(long long x, long long y)
-{
+const long long p = 1000000007;    // Prime nubmer, MOD
+
+long long pow(long long x, long long y) {
     long long result = 1;
-    while (y > 0)
-    {
-        if (y % 2)    //odd
-        {
+    
+    while (y > 0) {
+        if (y % 2) {    // Odd
             result *= x;
             result %= p;
         }
+        
         x *= x;
         x %= p;
         y /= 2;
     }
+    
     return result;
 }
 
-int main() 
-{
-    long long n, k;
+int main() {
+    long long n, k, result = A = B = 1;
     cin >> n >> k;
     
-    long long result = 1;
-    long long A = 1;
-    long long B = 1;
-    
-    //A = n!
-    for (long long i=1; i<=n; i++)
-    {
+    // A = n!
+    for (long long i = 1; i <= n; ++i) {
         A *= i;
         A %= p;
     }
     
-    //B = k!(n-k)!
-    for (long long i=1; i<=k; i++)
-    {
+    // B = k!(n - k)!
+    for (long long i = 1; i <= k; ++i) {
         B *= i;
         B %= p;
     }
-    for (long long i=1; i<=n-k; i++)
-    {
+    for (long long i = 1; i <= n - k; ++i) {
         B *= i;
         B %= p;
     }
     
-    //C = {k!(n-k)!}^(p-2)
-    long long C = pow(B, p-2);
+    // C = (k!(n - k)!)^(p - 2)
+    long long C = pow(B, p - 2);
     result = A * C % p;
     cout << result;
 }
