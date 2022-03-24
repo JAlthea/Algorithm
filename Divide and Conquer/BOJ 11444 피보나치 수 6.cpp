3974cb93a2c882714c963b61fda7c13dcb361446
Yongmin Jang <althea_@naver.com>
2022-03-24 19:38:15 +0900

    Modify code style

--- Divide and Conquer/BOJ 11444 피보나치 수 6.cpp
+++ Divide and Conquer/BOJ 11444 피보나치 수 6.cpp
@@ -2,26 +2,25 @@
 using namespace std;
 
 const long long m = 1e9 + 7;
-inline long long mod(long long k) { return k >= m ? k %= m : k; }
 
-long long fastFibo(long long n)
-{
+inline long long mod(long long k) { 
+	return k >= m ? k %= m : k; 
+}
+
+long long fastFibo(long long n) {
 	long long a = 1, b = 0, p = 0, q = 1, tmpA, tmpB, tmpP, tmpQ;
 
-	while (n > 0)
-	{
-		if (n % 2 == 0)
-		{
-			tmpP = mod((p*p) + (q*q));
-			tmpQ = mod((q*q) + (2 * p*q));
+	while (n > 0) {
+		if (n % 2 == 0) {
+			tmpP = mod((p * p) + (q * q));
+			tmpQ = mod((q * q) + (2 * p * q));
 			p = tmpP;
 			q = tmpQ;
 			n /= 2;
 		}
-		else
-		{
-			tmpA = mod((b*q) + (a*q) + (a*p));
-			tmpB = mod((b*p) + (a*q));
+		else {
+			tmpA = mod((b * q) + (a * q) + (a * p));
+			tmpB = mod((b * p) + (a * q));
 			a = tmpA;
 			b = tmpB;
 			--n;
