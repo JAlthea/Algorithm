5e3fd73af6d38f25cf84f647a7fd6eee4359f383
Yongmin Jang <althea_@naver.com>
2022-03-30 17:11:37 +0900

    Modify code style

--- Divide and Conquer/BOJ 1992 쿼드트리.cpp
+++ Divide and Conquer/BOJ 1992 쿼드트리.cpp
@@ -1,29 +1,37 @@
 #import <ios>
-int n; char d[64][64];
 
-void f(int x,int y,int m)
-{
-	int a = 0,b = 0;
-	for(int i=x; i<x+m; ++i)
-		for(int j=y; j<y+m; ++j)
+int n;
+char d[64][64];
+
+void f(int x, int y, int m) {
+	int a = b = 0;
+	
+	for (int i = x; i < x + m; ++i) {
+		for (int j = y; j < y + m; ++j) {
 			d[i][j] - 48 ? ++a : ++b;
-	if(a * b)
-	{
+		}
+	}
+	
+	if (a * b) {
 		printf("(");
-		f(x,y,m/2),f(x,y+m/2,m/2),f(x+m/2,y,m/2),f(x+m/2,y+m/2,m/2);
+		f(x, y, m / 2), f(x, y + m / 2, m / 2), f(x + m / 2, y, m / 2), f(x + m / 2, y + m / 2, m / 2);
 		printf(")");
 	}
-	else if(a)
+	else if(a) {
 		printf("1");
-	else
+	}
+	else {
 		printf("0");
+	}
 }
 
-int main()
-{
+int main() {
 	scanf("%d", &n);
-	for(int i=0; i<n; ++i)
-		for(int j=0; j<n; ++j)
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
 			scanf(" %c", &d[i][j]);
+		}
+	}
+	
 	f(0, 0, n);
 }
