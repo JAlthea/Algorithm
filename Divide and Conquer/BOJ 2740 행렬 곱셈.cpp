ebf49a393095b9502861984ed9739c54f4e04ad5
Yongmin Jang <althea_@naver.com>
2022-04-01 16:33:59 +0900

    Modify code style

--- Divide and Conquer/BOJ 2740 행렬 곱셈.cpp
+++ Divide and Conquer/BOJ 2740 행렬 곱셈.cpp
@@ -1,20 +1,27 @@
 #import <ios>
 
-int a[100][100], b[100][100], n, m, k, i, j, x, y;
-int main()
-{
+int main() {
+  int n, m, k, a[100][100], b[100][100];
+
   scanf("%d%d", &n, &m);
-  for(i=0; i<n; ++i)
-    for(j=0; j<m; ++j)
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
       scanf("%d", &a[i][j]);
-	
+    }
+  }
+
   scanf("%d%d", &m, &k);
-  for(i=0; i<m; ++i)
-    for(j=0; j<k; ++j)
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < k; ++j) {
       scanf("%d", &b[i][j]);
+    }
+  }
       
-  for(x=0; x<n; ++x, puts(""))
-    for(y=0; y<k; ++y, printf("%d ", j))
-      for(j=0, i=0; i<m; ++i)
+  for (int x = 0; x < n; ++x, puts("")) {
+    for (int y = 0; y < k; ++y, printf("%d ", j)) {
+      for (int j = 0, i = 0; i < m; ++i) {
         j += a[x][i] * b[i][y];
+      }
+    }
+  }
 }
