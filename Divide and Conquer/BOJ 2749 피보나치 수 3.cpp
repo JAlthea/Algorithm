0e04878c4f124f0d4c504d50390563f90e091ff2
Yongmin Jang <althea_@naver.com>
2022-04-02 12:43:18 +0900

    Modify code style

--- Divide and Conquer/BOJ 2749 피보나치 수 3.cpp
+++ Divide and Conquer/BOJ 2749 피보나치 수 3.cpp
@@ -1,13 +1,15 @@
 #include <cstdio>
 
+const int p = 1'500'000;
+int d[p + 1] = { 0, 1 };
 long long n;
-int d[1500001] = { 0, 1 }, p = 1500000;
 
-int main()
-{
+int main() {
     scanf("%lld", &n);
-    for (int i=2; i<p; ++i)
+    
+    for (int i = 2; i < p; ++i) {
         d[i] = (d[i - 1] + d[i - 2]) % 1000000;
-    int t = (int)(n % 1500000);
-    printf("%d", d[t]);
+    }
+    
+    printf("%d", d[n % p]);
 }
