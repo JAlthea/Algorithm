d09f7c5f9db82b72667ed1929e6f351c27905c34
Yongmin Jang <althea_@naver.com>
2022-04-03 19:00:19 +0900

    Modify code style

--- Divide and Conquer/BOJ 6549 히스토그램에서 가장 큰 직사각형.cpp
+++ Divide and Conquer/BOJ 6549 히스토그램에서 가장 큰 직사각형.cpp
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    while (1)
-    {
+int main() {
+    while (true) {
         int n;
         scanf("%d", &n);
-        if (n == 0)
+        if (n == 0) {
             break;
+        }
 
         vector<int> h(n + 1, 0);
-        for (int i=0; i<n; ++i)
+        for (int i = 0; i < n; ++i) {
             scanf("%d", &h[i]);
+        }
     
         stack<int> remains;
         long long result = 0;
-        for (int i=0; i<n+1; ++i)
-        {
-            while (!remains.empty() && h[remains.top()] >= h[i])
-            {
-                int now = remains.top(); 
-                remains.pop();
+        
+        for (int i = 0; i < n + 1; ++i) {
+            while (!remains.empty() && h[remains.top()] >= h[i]) {
+                int now = remains.top(); remains.pop();
                 int width = -1;
-                if (remains.empty())
+                if (remains.empty()) {
                     width = i;
-                else
+                }
+                else {
                     width = (i - remains.top() - 1);
+                }
             
                 result = max(result, (long long)width * h[now]);
             }
