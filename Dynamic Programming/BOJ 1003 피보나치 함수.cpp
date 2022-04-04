f64d748ae44c5ea072c2d69d6d816216ad8ab18a
Yongmin Jang <althea_@naver.com>
2022-04-04 17:26:54 +0900

    Modify code style

--- Dynamic Programming/BOJ 1003 피보나치 함수.cpp
+++ Dynamic Programming/BOJ 1003 피보나치 함수.cpp
@@ -1,25 +1,22 @@
 #include <cstdio>
 
-int d[40][2], n, T, i;
+int d[40][2], n;
 
-void f(int n)
-{
+void dp(int n) {
     d[0][0] = 1;
     d[1][1] = 1;
-    for (i=2; i<=n; i++)
-    {
-        d[i][0] = d[i-1][0] + d[i-2][0];
-        d[i][1] = d[i-1][1] + d[i-2][1];
+    for (int i = 2; i <= n; ++i) {
+        d[i][0] = d[i - 1][0] + d[i - 2][0];
+        d[i][1] = d[i - 1][1] + d[i - 2][1];
     }
 }
 
-int main()
-{
-    scanf("%d", &T);
-    while (T--)
-    {
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
         scanf("%d", &n);
-        f(n);
+        dp(n);
         printf("%d %d\n", d[n][0], d[n][1]);
     }
 }
