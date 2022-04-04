f0902bdfc0d6888f284033fc38eaa38974b42ed3
Yongmin Jang <althea_@naver.com>
2022-04-05 00:36:02 +0900

    Modify code style

--- Dynamic Programming/BOJ 10844 쉬운 계단 수.cpp
+++ Dynamic Programming/BOJ 10844 쉬운 계단 수.cpp
@@ -1,23 +1,38 @@
 #include <cstdio>
-#define D 1000000000
 
-int d[10], c[10], n, i, j, s;
+const int M = 1e9;
+int d[10], c[10], n, s;
 
-int main()
-{
-	for(i=1; i<10; i++) d[i]++, c[i]++;
-	scanf("%d", &n);
-	for(i=2; i<=n; i++)
-	{
-		for(j=0; j<10; j++)
-		{
-			if(!j) d[j] = c[1];
-			else if(j==9) d[j] = c[8];
-			else d[j] = c[j-1] + c[j+1];
-			d[j] %= D;
-		}
-		for(j=0; j<10; j++) c[j] = d[j];
+int main() {
+	for (int i = 1; i < 10; ++i) { 
+		++d[i], ++c[i];
 	}
-	for(i=0; i<10; i++) s = (s + d[i]) % D;
-	printf("%d", s % D);
+	
+	scanf("%d", &n);
+	
+	for (int i = 2; i <= n; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			if (!j) { 
+				d[j] = c[1];
+			}
+			else if (j == 9) {
+				d[j] = c[8];
+			}
+			else {
+				d[j] = c[j - 1] + c[j + 1];
+			}
+			
+			d[j] %= M;
+		}
+		
+		for (int j = 0; j < 10; ++j) {
+			c[j] = d[j];
+		}
+	}
+	
+	for (int i = 0; i < 10; ++i) {
+		s = (s + d[i]) % M;
+	}
+	
+	printf("%d", s);
 }
