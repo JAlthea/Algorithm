ef136b811c94b808036976fed772e9bc69a7b6d9
Yongmin Jang <althea_@naver.com>
2022-04-07 05:33:21 +0900

    Modify code style

--- Dynamic Programming/BOJ 11053 가장 긴 증가하는 부분 수열.cpp
+++ Dynamic Programming/BOJ 11053 가장 긴 증가하는 부분 수열.cpp
@@ -1,19 +1,27 @@
 #include <bits/stdc++.h>
-int main()
-{
-	int n, d[1001] = { 0 }, s[1001] = { 0 };
-	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &d[i]);
 
-	for (int i = 1; i <= n; i++)
-	{
+int n, d[1001], s[1001];
+
+int main() {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; ++i) {
+		scanf("%d", &d[i]);
+	}
+
+	for (int i = 1; i <= n; ++i) {
 		int now = d[i];
 		int maxCount = 0;
-		for (int j = 1; j < i; j++)
-			if (d[i] > d[j])
-				maxCount = std::max(maxCount, s[j]);
+		
+		for (int j = 1; j < i; ++j) {
+			if (d[i] <= d[j]) {
+				continue;
+			}
+			
+			maxCount = std::max(maxCount, s[j]);
+		}
+		
 		s[i] = maxCount + 1;
 	}
+	
 	printf("%d", *std::max_element(s, s + n + 1));
 }
