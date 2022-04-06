795ef9f5c696926c7ddb1037b5a334f12eb0c989
Yongmin Jang <althea_@naver.com>
2022-04-06 23:22:10 +0900

    Modify code style

--- Dynamic Programming/BOJ 11049 행렬 곱셈 순서.cpp
+++ Dynamic Programming/BOJ 11049 행렬 곱셈 순서.cpp
@@ -2,22 +2,20 @@
 using namespace std;
 
 int n, t, result[501][501], d[501];
-int main()
-{
-    cin >> n;
-    cin >> d[0];
-    for (int i = 1; i < n; i++)
+
+int main() {
+    cin >> n >> d[0];
+    for (int i = 1; i < n; ++i) {
         cin >> d[i] >> t;
+    }
     cin >> d[n];
 
-    for (int i = 1; i <= n; i++)
-    {
-        for (int s = 1; s + i <= n; s++)
-        {
+    for (int i = 1; i <= n; ++i) {
+        for (int s = 1; s + i <= n; ++s) {
             int e = s + i;
             result[s][e] = INT_MAX;
-            for (int c = s; c < e; c++)
-            {
+            
+            for (int c = s; c < e; ++c) {
                 result[s][e] = min(result[s][e], result[s][c] + result[c + 1][e] + d[s - 1] * d[c] * d[e]);
             }
         }
