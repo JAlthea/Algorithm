d084087647e9ab8feeed8dc5d8e502f8a8704402
Yongmin Jang <althea_@naver.com>
2022-04-09 01:11:51 +0900

    Modify code style

--- Dynamic Programming/BOJ 11066 파일 합치기.cpp
+++ Dynamic Programming/BOJ 11066 파일 합치기.cpp
@@ -1,30 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
         int k;
         int c[501] = { 0 }, d[501][501] = { { 0 } }, t;
+        
         cin >> k;
-        for (int i=1; i<=k; i++)
-        {
+        for (int i = 1; i <= k; ++i) {
             cin >> t;
-            c[i] = c[i-1] + t;
+            c[i] = c[i - 1] + t;
         }
         
-        for (int i=1; i<=k; i++)
-        {
-            for (int s=1; s+i<=k; s++)
-            {
+        for (int i = 1; i <= k; ++i) {
+            for (int s = 1; s + i <= k; ++s) {
                 int e = s + i;
                 d[s][e] = INT_MAX;
-                for (int r=s; r<e; r++)
-                {
-                    d[s][e] = min(d[s][e], d[s][r] + d[r+1][e] + c[e] - c[s-1]);
+                
+                for (int r = s; r < e; ++r) {
+                    d[s][e] = min(d[s][e], d[s][r] + d[r + 1][e] + c[e] - c[s - 1]);
                 }
             }
         }
