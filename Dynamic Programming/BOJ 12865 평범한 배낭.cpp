f777d7b675efec1ecf2afa32ebf43a9f37dc2551
Yongmin Jang <althea_@naver.com>
2022-04-12 11:14:12 +0900

    Modify code style

--- Dynamic Programming/BOJ 12865 평범한 배낭.cpp
+++ Dynamic Programming/BOJ 12865 평범한 배낭.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int n, k, d[101][100001], w[101], v[101];
-int main()
-{
+
+int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> n >> k;
-    for (int i=1; i<=n; i++)
-        cin >> w[i] >> v[i];
     
-    for (int i=1; i<=n; i++)
-    {
-        for (int j=1; j<=k; j++)
-        {
+    cin >> n >> k;
+    for (int i = 1; i <= n; ++i) {
+        cin >> w[i] >> v[i];
+    }
+    
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= k; ++j) {
             d[i][j] = d[i - 1][j];
-            if (j >= w[i])
+            
+            if (j >= w[i]) {
                 d[i][j] = max(d[i][j], d[i - 1][j - w[i]] + v[i]);
+            }
         }
     }
     
