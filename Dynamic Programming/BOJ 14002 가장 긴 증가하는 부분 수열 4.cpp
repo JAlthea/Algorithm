ce94d07d9fb0a57d000a656cedd4d6ad3e95285b
Yongmin Jang <althea_@naver.com>
2022-04-15 13:39:01 +0900

    Modify code style

--- Dynamic Programming/BOJ 14002 가장 긴 증가하는 부분 수열 4.cpp
+++ Dynamic Programming/BOJ 14002 가장 긴 증가하는 부분 수열 4.cpp
@@ -2,37 +2,40 @@
 
 int n, d[1001], s[1001];
 
-int main()
-{
+int main() {
 	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; ++i) {
 		scanf("%d", &d[i]);
+	}
 
-	for (int i = 1; i <= n; i++)
-	{
+	for (int i = 1; i <= n; ++i) {
 		int now = d[i];
 		int maxCount = 0;
-		for (int j = 1; j < i; j++)
-			if (d[i] > d[j])
+		for (int j = 1; j < i; ++j) {
+			if (d[i] > d[j]) {
 				maxCount = std::max(maxCount, s[j]);
+			}
+		}
 
 		s[i] = maxCount + 1;
 	}
+	
 	int maxLength = *std::max_element(s, s + n + 1);
 	printf("%d\n", maxLength);
 
 	std::stack<int> st;
-	for (int i = n; i > 0; i--)
-	{
-		if (maxLength == 0)
+	for (int i = n; i > 0; --i) {
+		if (maxLength == 0) {
 			break;
-		if (s[i] == maxLength) 
-		{
+		}
+		
+		if (s[i] == maxLength) {
 			st.push(d[i]);
-			maxLength--;
+			--maxLength;
 		}
 	}
-	int size = st.size();
-	for (int i = 0; i < size; st.pop(), i++)
-		printf("%d ", st.top());
+	
+	while (!st.empty()) {
+		printf("%d ", st.top()); st.pop();
+	}
 }
