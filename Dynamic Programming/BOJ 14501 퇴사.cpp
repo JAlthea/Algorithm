557fc9bdca5ad5669a79908842c6e9762fe42625
Yongmin Jang <althea_@naver.com>
2022-04-17 12:46:51 +0900

    Modify code style

--- Dynamic Programming/BOJ 14501 퇴사.cpp
+++ Dynamic Programming/BOJ 14501 퇴사.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int n, d[16], t[16], p[16];
 
-int main()
-{
+int main() {
 	cin >> n;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; ++i) {
 		cin >> t[i] >> p[i];
+	}
 
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; ++i) {
 		d[i + 1] = max(d[i + 1], d[i]);
-		if (i + t[i] <= n) d[i + t[i]] = max(d[i + t[i]], d[i] + p[i]);
+		if (i + t[i] <= n) {
+			d[i + t[i]] = max(d[i + t[i]], d[i] + p[i]);
+		}
 	}
 
 	cout << d[n];
