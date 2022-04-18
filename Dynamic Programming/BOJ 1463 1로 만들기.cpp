d4db7f03853cd0f4705b0e6fde3816ce33557d7c
Yongmin Jang <althea_@naver.com>
2022-04-18 15:05:49 +0900

    Modify code style

--- Dynamic Programming/BOJ 1463 1로 만들기.cpp
+++ Dynamic Programming/BOJ 1463 1로 만들기.cpp
@@ -1,12 +1,10 @@
 #include <ios>
 
-int f(int n)
-{
+inline int f(int n) {
     return n < 2 ? 0 : std::min(f(n / 2) + n % 2, f(n / 3) + n % 3) + 1;
 }
 
-int main()
-{
+int main() {
     int n;
     scanf("%d", &n);
     printf("%d", f(n));
