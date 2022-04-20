7c572c40d16038f881b85d9886e8e3a17fb9a152
Yongmin Jang <althea_@naver.com>
2022-04-21 08:56:06 +0900

    Modify code style

--- Dynamic Programming/BOJ 15486 퇴사 2.cpp
+++ Dynamic Programming/BOJ 15486 퇴사 2.cpp
@@ -3,20 +3,30 @@ using namespace std;
 
 int n, d[1500001], t[1500001], p[1500001];
 
-int f(int index)
-{
-	if (index == n) return 0;
-	if (index > n) return INT_MIN;
-	if (d[index] != -1) return d[index];
+int f(int index) {
+	if (index == n) { 
+		return 0;
+	}
+	
+	if (index > n) { 
+		return INT_MIN;
+	}
+	
+	if (d[index] != -1) { 
+		return d[index];
+	}
+	
 	int &ret = d[index];
 	return ret = max(f(index + 1), f(index + t[index]) + p[index]);
 }
 
-int main()
-{
+int main() {
 	memset(d, -1, sizeof(d));
+	
 	cin >> n;
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; ++i) {
 		cin >> t[i] >> p[i];
+	}
+	
 	cout << f(0);
 }
