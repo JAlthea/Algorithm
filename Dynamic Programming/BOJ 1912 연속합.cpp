c87dbe04b3a49ec1160dfbf54aca8ec2c0d8cf24
Yongmin Jang <althea_@naver.com>
2022-04-25 14:22:01 +0900

    Modify code style

--- Dynamic Programming/BOJ 1912 연속합.cpp
+++ Dynamic Programming/BOJ 1912 연속합.cpp
@@ -2,13 +2,17 @@
 
 int n, d[100001];
 
-int main()
-{
+int main() {
     d[0] = -1e9;
+    
     scanf("%d", &n);
-    for (int i=1; i<=n; i++)
+    for (int i = 1; i <= n; ++i) {
         scanf("%d", &d[i]);
-    for (int i=1; i<=n; i++)
-        d[i] = std::max(d[i-1] + d[i], d[i]);
+    }
+    
+    for (int i = 1; i <= n; ++i) {
+        d[i] = std::max(d[i - 1] + d[i], d[i]);
+    }
+    
     printf("%d", *std::max_element(d, d + n + 1));
 }
