630c6d99c0efb45a9f6e812994c37f4766051ea6
Yongmin Jang <althea_@naver.com>
2022-04-29 09:14:21 +0900

    Modify code style

--- Dynamic Programming/BOJ 2293 동전 1.cpp
+++ Dynamic Programming/BOJ 2293 동전 1.cpp
@@ -3,16 +3,19 @@
 
 int n, k, i, j, d[10001], s[101];
 
-int main()
-{
-	for (scanf("%d%d", &n, &k); i<n; ++i)
+int main() {
+	for (scanf("%d%d", &n, &k); i < n; ++i) {
 		scanf("%d", &s[i]);
+	}
+	
 	std::sort(s,s + n);
 
 	d[0] = 1;
-	for (i=0; i<n; ++i)
-		for(j=s[i]; j<=k; ++j)	
+	for (i = 0; i < n; ++i) {
+		for (j = s[i]; j <= k; ++j) {
 			d[j] += d[j - s[i]];
+		}
+	}
 
 	printf("%d", d[k]);
 }
