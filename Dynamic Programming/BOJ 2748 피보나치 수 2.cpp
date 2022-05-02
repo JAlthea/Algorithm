858aff8e7ec6be66237ec63719d937bee83b43f0
Yongmin Jang <althea_@naver.com>
2022-05-02 16:18:46 +0900

    Modify coding style

--- Dynamic Programming/BOJ 2748 피보나치 수 2.cpp
+++ Dynamic Programming/BOJ 2748 피보나치 수 2.cpp
@@ -1,10 +1,14 @@
 #include <cstdio>
+
 long long d[91], n;
-int main() 
-{
+
+int main() {
 	scanf("%d", &n);
+	
 	d[1] = 1;
-	for (int i=2; i<=n; i++)
-        d[i] = d[i-1] + d[i-2];
+	for (int i = 2; i <= n; ++i) {
+        	d[i] = d[i - 1] + d[i - 2];
+	}
+	
 	printf("%lld", d[n]);
 }
